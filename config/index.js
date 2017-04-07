5b7717fd2f4b677dffa8e29caeaca41c68cee3cc
yibuyisheng <yibuyisheng@163.com>
2017-04-07 11:48:21 +0800

    端口号改回来

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8083,
+    port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
