487387573d76067d0c315f09c3f4751f0f54946c
feibinyang <feibinyang@gmail.com>
2017-04-16 02:06:41 +0800

    Dropdown组件

---
+++ demo/cases/Dropdown.vue
@@ -0,0 +1,60 @@
+<template>
+  <article>
+    <h1><code>&lt;veui-button&gt;</code></h1>
+    <p>
+      <veui-dropdown
+        ui="primary"
+        label="操作操作操作操作操作操作"
+        icon="chevron"
+        :options="options"></veui-dropdown>
+    </p>
+    <p>
+      <veui-dropdown
+        ui="link"
+        label="操作"
+        :options="options"></veui-dropdown>
+    </p>
+  </article>
+</template>
+
+<script>
+import bus from '../bus'
+import Dropdown from '@/components/Dropdown'
+
+export default {
+  name: 'dropdown',
+  components: {
+    'veui-dropdown': Dropdown
+  },
+  data () {
+    return {
+      options: [
+        {
+          label: '新建新建新建新建新建新建新建'
+        },
+        {
+          label: '编辑',
+          disabled: true
+        },
+        {
+          label: '删除'
+        }
+      ]
+    }
+  },
+  mounted () {
+    this.$children.forEach(child => {
+      child.$on('click', () => {
+        bus.$emit('log', child.$el.getAttribute('ui'))
+      })
+    })
+  }
+}
+</script>
+
+<style scoped>
+p {
+  margin: 10px 0;
+}
+
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="veui-dropdown" :ui="ui">
+    <veui-overlay :open="expanded"
+      :options="overlay"
+      v-clickoutside="clickoutside">
+      <veui-button
+        :ui="ui"
+        :class="{'veui-button-chevron': icon === 'chevron'}"
+        :disabled="disabled"
+        @click.stop="expanded = !expanded"
+        slot="target">
+        <slot name="dropdown-button" :label="label">
+          <icon :name="`${icon}-${expanded ? 'up' : 'down'}`"></icon>
+          <span>{{ label }}</span>
+        </slot>
+      </veui-button>
+      <div class="veui-dropdown-options">
+        <div v-for="(option, index) in options"
+          :key="index"
+          class="veui-dropdown-option"
+          :class="{
+            'veui-dropdown-option-disabled': option.disabled
+          }"
+          @click.stop="clickHandler(index)">
+          <slot name="dropdown-option"
+            v-bind="option"
+            :index="index">
+            <span>{{ option.label }}</span>
+          </slot>
+        </div>
+      </div>
+    </veui-overlay>
+  </div>
+</template>
+
+<script>
+import Icon from './Icon'
+import Button from './Button'
+import Overlay from './Overlay'
+import 'vue-awesome/icons/caret-down'
+import 'vue-awesome/icons/caret-up'
+import 'vue-awesome/icons/chevron-down'
+import 'vue-awesome/icons/chevron-up'
+
+export default {
+  name: 'veui-dropdown',
+  components: {
+    'icon': Icon,
+    'veui-button': Button,
+    'veui-overlay': Overlay
+  },
+  props: {
+    ui: String,
+    label: String,
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: 'caret'
+    },
+    options: Array
+  },
+  data () {
+    return {
+      expanded: false,
+      overlay: {
+        attachment: 'top left',
+        targetAttachment: 'bottom left',
+        offset: '-3px 0'
+      }
+    }
+  },
+  methods: {
+    clickHandler (index) {
+      this.expanded = !this.expanded
+      this.$emit('click', index)
+    },
+    clickoutside () {
+      if (this.expanded) {
+        this.expanded = !this.expanded
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import "../styles/theme-default/lib.less";
+.veui-dropdown {
+  .veui-button {
+    width: 110px;
+    padding: 11px 15px;
+    position: relative;
+    > span {
+      display: inline-block;
+      width: 100%;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      text-align: left;
+    }
+    > svg {
+      width: 13px;
+      height: 13px;
+      position: absolute;
+      right: 15px;
+      top: 11px;
+      & + span {
+        padding-right: 16px;
+      }
+    }
+    &.veui-button-chevron {
+      > svg {
+        width: 14px;
+        height: 8px;
+        top: 14px;
+      }
+    }
+  }
+}
+.veui-dropdown-options {
+  min-width: 110px;
+  max-height: 280px;
+  overflow-y: auto;
+  background-color: #fff;
+  border: 1px solid @veui-gray-color-sup-2;
+  border-radius: 2px;
+  .veui-dropdown-option {
+    cursor: pointer;
+    width: 100%;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 10px;
+    &:hover {
+      background-color: @veui-theme-color-sup-4;
+    }
+    &.veui-dropdown-option-selected {
+      color: @veui-theme-color-primary;
+      font-weight: @veui-font-weight-bold;
+    }
+    &.veui-dropdown-option-disabled {
+      background-color: #fff;
+      color: @veui-gray-color-weak;
+      cursor: not-allowed;
+    }
+  }
+}
+</style>
