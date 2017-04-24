6278a47d790cd0490ad850dc2f8939f861a596c1
feibinyang <feibinyang@gmail.com>
2017-04-24 23:34:55 +0800

    none木有意义

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -97,7 +97,7 @@ export default {
         constraints: [
           {
             to: 'scrollParent',
-            attachment: 'together none'
+            attachment: 'together'
           }
         ]
       }
