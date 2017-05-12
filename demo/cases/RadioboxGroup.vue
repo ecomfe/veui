83dd74e6b8534c6139677647bad127f82e9afa93
Justineo <justice360@gmail.com>
2017-05-12 16:05:58 +0800

    add CheckboxGroup/RadioboxGroup

---
+++ demo/cases/RadioboxGroup.vue
@@ -0,0 +1,56 @@
+<template>
+  <article>
+    <h1><code>&lt;veui-radioboxgroup&gt;</code></h1>
+    <p>
+      <veui-radioboxgroup :items="data1" name="111" v-model="picked1">
+        <template scope="props">{{ props.label }}</template>
+      </veui-radioboxgroup>
+    </p>
+    <p>value: {{ picked1 }}</p>
+    <p>
+      <veui-radioboxgroup :items="data2" ui="small" v-model="picked2">
+        <template scope="props">{{ props.label }}</template>
+      </veui-radioboxgroup>
+    </p>
+    <p>value: {{ picked2 }}</p>
+  </article>
+</template>
+
+<script>
+import RadioboxGroup from '@/components/RadioboxGroup'
+
+export default {
+  name: 'radioboxgroup-demo',
+  components: {
+    'veui-radioboxgroup': RadioboxGroup
+  },
+  data () {
+    return {
+      picked1: 'Akiyama Mio',
+      picked2: 'Nakano Azusa',
+      data1: [
+        {
+          value: 'Hirasawa Yui', label: 'Hirasawa Yui'
+        },
+        {
+          value: 'Akiyama Mio', label: 'Akiyama Mio'
+        },
+        {
+          value: 'Nakano Azusa', label: 'Nakano Azusa'
+        }
+      ],
+      data2: [
+        {
+          value: 'Hirasawa Yui', label: 'Hirasawa Yui'
+        },
+        {
+          value: 'Akiyama Mio', label: 'Akiyama Mio'
+        },
+        {
+          value: 'Nakano Azusa', label: 'Nakano Azusa'
+        }
+      ]
+    }
+  }
+}
+</script>
