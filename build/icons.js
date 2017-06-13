ccb2ef8aa1df514c685bb8a463857c3773e1decc
yibuyisheng <yibuyisheng@163.com>
2017-06-13 14:18:23 +0800

    icon register

--- build/icons.js
+++ build/icons.js
@@ -3,5 +3,5 @@ const path = require('path');
 const icons = require('../assets/icons.json');
 
 const ICON_PATH = path.resolve(__dirname, '../src/icons.js');
-fs.writeFileSync(ICON_PATH, `/* eslint-disable */\nimport Icon from 'vue-awesome/components/Icon'\nIcon.register(${JSON.stringify(icons)})\n`);
+fs.writeFileSync(ICON_PATH, `/* eslint-disable */\nimport Icon from './components/Icon'\nIcon.register(${JSON.stringify(icons)})\n`);
 console.log(Object.keys(icons).length + ' icon modules generated.');
