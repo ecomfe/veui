695495f68fd0fe09297274c14caa2ab27f44768c
qbatyqi <qbaty.qi@gmail.com>
2017-06-28 21:57:16 +0800

    add case & simple ui

--- demo/cases/Tabs.vue
+++ demo/cases/Tabs.vue
@@ -3,19 +3,27 @@
     <h1><code>&lt;veui-tabs&gt;</code></h1>
     <section>
       <h2>默认样式：</h2>
-      <veui-tabs v-bind="attrs" v-model="defaultValue1"></veui-tabs>
-      <h2>slot样式：</h2>
-      <veui-tabs v-bind="attrs" v-model="defaultValue1">
+      <veui-tabs ui="default" :active.sync="tabOneActive" >
         <veui-tab label="Tab1" name="tab1"> This is Tab1 </veui-tab>
         <veui-tab label="Tab2" name="tab2"> This is Tab2 </veui-tab>
+        <veui-tab label="Tab3" name="tab3"> This is Tab3 </veui-tab>
       </veui-tabs>
+      <div>当前选的 Tab 名是：{{tabOneActive}}</div>
+    </section>
+    <section>
+      <h2>简单样式：</h2>
+      <veui-tabs ui="simple" :active.sync="tabThreeActive" >
+        <veui-tab label="Tab1" name="tab1"> This is Tab1 </veui-tab>
+        <veui-tab label="Tab2" name="tab2"> This is Tab2 </veui-tab>
+        <veui-tab label="Tab3" name="tab3"> This is Tab3 </veui-tab>
+      </veui-tabs>
+      <div>当前选的 Tab 名是：{{tabThreeActive}}</div>
     </section>
   </article>
 </template>
 
 <script>
 import Icon from '@/components/Icon'
-// import bus from '../bus'
 import Tabs from '@/components/Tabs/Tabs'
 import Tab from '@/components/Tabs/Tab'
 
@@ -28,10 +36,9 @@ export default {
   },
   data () {
     return {
-      icon: true,
-      defaultValue1: null,
-      attrs: {
-      }
+      tabOneActive: '',
+      tabTwoActive: 1,
+      tabThreeActive: 2
     }
   },
   mounted () {
--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -3,7 +3,19 @@ import Tab from './Tab'
 export default {
   name: 'veui-tabs',
 
-  props: ['ui', 'type', 'active'],
+  props: {
+    ui: {
+      type: String,
+      default: 'default'
+    },
+    type: {
+      type: String
+    },
+    active: {
+      type: [String, Number],
+      default: 0
+    }
+  },
 
   data () {
     return {
@@ -14,7 +26,7 @@ export default {
 
   render () {
     return (
-      <div class="veui-tabs-wrap">
+      <div class="veui-tabs-wrap" ui={this.ui}>
         <div class="veui-tabs-nav">
           <div class="veui-tabs-list">
             {
@@ -34,19 +46,26 @@ export default {
     )
   },
 
+  computed: {
+    tabsName () {
+      return this.tabs.map(item => item.name)
+    }
+  },
+
   methods: {
     add (tab) {
       this.tabs.push(tab)
     },
 
     setCurrent (value) {
-      if (!this.tabs.length) return
-      let values = this.tabs.map(item => item.name)
-      if (value) {
-        if (values.indexOf(value) !== -1) {
-          this.current = value
-        }
-      } else if (values.length) {
+      let values = this.tabsName
+
+      if (!values.length) return
+      if (typeof value === 'number') {
+        this.current = values[value]
+      } else if (typeof value === 'string' && values.indexOf(value) !== -1) {
+        this.current = value
+      } else {
         this.current = values[0]
       }
     }
@@ -54,12 +73,15 @@ export default {
 
   watch: {
     current (val) {
-      this.$emit('change', val)
+      this.$emit('update:active', val)
+    },
+
+    active (val) {
+      this.setCurrent(val)
     }
   },
 
   mounted () {
-    console.log(111)
     this.setCurrent(this.active)
   },
 
@@ -73,44 +95,76 @@ export default {
 @import "../../styles/theme-default/variables.less";
 
 .veui-tabs-wrap {
+
   .veui-tabs-nav {
-    overflow: hidden;
     position: relative;
     margin-bottom: -1px;
   }
 
   .veui-tabs-list {
     overflow: hidden;
+  }
 
-    .veui-tabs-item {
-      list-style: none;
-      color: @veui-gray-color-normal;
-      float: left;
-      cursor: pointer;
-      text-align: center;
-      padding: 0 25px;
-      font-size: 14px;
-      line-height: 36px;
-      height: 36px;
-      border: 1px solid @veui-gray-color-sup-1;
-      border-radius: 4px 4px 0 0;
-      margin-right: 6px;
-      background: #FFF;
+  .veui-tabs-item {
+    list-style: none;
+    color: @veui-gray-color-normal;
+    float: left;
+    text-align: center;
+    padding: 0 25px;
+    font-size: 14px;
+    line-height: 36px;
+    height: 36px;
+    border: 1px solid @veui-gray-color-sup-1;
+    border-radius: 4px 4px 0 0;
+    margin-right: 6px;
+    cursor: pointer;
+    background: @veui-gray-color-sup-5;
 
-      &:hover {
-        color: @veui-theme-color-hover;
-      }
+    &:hover {
+      color: @veui-theme-color-hover;
+    }
 
-      &.active {
-        border-bottom: none;
-        color: @veui-theme-color-primary;
-      }
+    &.active {
+      border-bottom: none;
+      background: #fff;
+      color: @veui-theme-color-primary;
     }
   }
 
   .veui-tab-content {
     border: 1px solid @veui-gray-color-sup-1;
     padding: 20px;
+    min-height: 200px;
+  }
+
+  &[ui~="simple"] {
+    .veui-tabs-list {
+      position: relative;
+      margin-bottom: -1px;
+    }
+
+    .veui-tabs-item {
+      min-width: 80px;
+      border: none;
+      border-radius: 0;
+      background: #fff;
+      border-bottom:1px solid @veui-gray-color-sup-1;
+
+
+      &.active {
+        border-bottom:1px solid @veui-theme-color-primary;
+      }
+      
+    }
+    
+    .veui-tabs-nav {
+      border-bottom:1px solid @veui-gray-color-sup-1;
+      margin-bottom: 20px;
+    }
+
+    .veui-tab-content {
+      border: none;
+    }
   }
   
 }
