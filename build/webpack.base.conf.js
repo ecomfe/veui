7cabeae74ca7191a4708cfaf87cfd30b16a12fdf
Justineo <justice360@gmail.com>
2017-06-30 17:18:26 +0800

    remove moment locales while building

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -62,5 +63,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^$/)
+  ]
 }
