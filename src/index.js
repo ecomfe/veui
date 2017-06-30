dbb96d1579eade91950bc627f3c6ae2c0f105f43
yibuyisheng <yibuyisheng@163.com>
2017-06-30 17:22:26 +0800

    export in index (#73)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ export default {
   install () {
   }
 }
+
+export { default as Alert } from './components/Alert'
+export { default as AlertBox } from './components/AlertBox'
+export { default as Breadcrumb } from './components/Breadcrumb'
+export { default as BreadcrumbItem } from './components/BreadcrumbItem'
+export { default as Button } from './components/Button'
+export { default as Calendar } from './components/Calendar'
+export { default as Checkbox } from './components/Checkbox'
+export { default as CheckboxGroup } from './components/CheckboxGroup'
+export { default as ConfirmBox } from './components/ConfirmBox'
+export { default as DatePicker } from './components/DatePicker'
+export { default as Dialog } from './components/Dialog'
+export { default as Dropdown } from './components/Dropdown'
+export { default as Icon } from './components/Icon'
+export { default as Input } from './components/Input'
+export { default as Link } from './components/Link'
+export { default as Overlay } from './components/Overlay'
+export { default as Pager } from './components/Pager'
+export { default as PromptBox } from './components/PromptBox'
+export { default as Radiobox } from './components/Radiobox'
+export { default as RadioboxGroup } from './components/RadioboxGroup'
+export { default as Toast } from './components/Toast'
+export { default as ToastList } from './components/ToastList'
+export { default as Tooltip } from './components/Tooltip'
+export { default as Uploader } from './components/Uploader'
