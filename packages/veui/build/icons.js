2434b0f46be36c9df529aafdcd6cc0a69d3eb27e
yibuyisheng <yibuyisheng@163.com>
2017-09-06 17:21:23 +0800

    breadcrumb 和 dialog 系列样式

--- packages/veui/build/icons.js
+++ packages/veui/build/icons.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const icons = require('../assets/icons.json');
+const icons = require('../../veui-theme-x/assets/icons.json');
 
 const ICON_PATH = path.resolve(__dirname, '../src/icons.js');
 fs.writeFileSync(ICON_PATH, `/* eslint-disable */\nimport Icon from './components/Icon'\nIcon.register(${JSON.stringify(icons)})\n`);
