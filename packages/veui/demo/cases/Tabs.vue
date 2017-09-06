29ad12b75189451f2223650c31483ed59f776532
qbatyqi <qbaty.qi@gmail.com>
2017-09-06 15:48:42 +0800

    add v4 tab style

--- packages/veui/demo/cases/Tabs.vue
+++ packages/veui/demo/cases/Tabs.vue
@@ -5,18 +5,54 @@
       <h2>默认样式：</h2>
       <p>当前标签 <code>{{active}}</code></p>
       <p>当前序号 <code>{{index0 + 1}}</code></p>
-      <veui-tabs ui="default" :active.sync="active" :index.sync="index0">
-        <veui-tab label="回答" name="answers"><h1>This is Tab1</h1></veui-tab>
-        <veui-tab label="文章" name="articles"><h1>This is Tab2</h1></veui-tab>
-        <veui-tab label="分享" name="shares"><h1>This is Tab3</h1></veui-tab>
+      <veui-tabs ui="large" :active.sync="active" :index.sync="index0">
+        <veui-tab label="回答问题" name="answers"></veui-tab>
+        <veui-tab label="文章评论" name="articles"></veui-tab>
+        <veui-tab label="分享朋友圈" name="shares"></veui-tab>
+      </veui-tabs>
+
+      <veui-tabs :active.sync="active" :index.sync="index0">
+        <veui-tab label="回答问题" name="answers"></veui-tab>
+        <veui-tab label="文章评论" name="articles"></veui-tab>
+        <veui-tab label="分享朋友圈" name="shares"></veui-tab>
+      </veui-tabs>
+
+      <veui-tabs ui="small" :active.sync="active" :index.sync="index0">
+        <veui-tab label="回答问题" name="answers"></veui-tab>
+        <veui-tab label="文章评论" name="articles"></veui-tab>
+        <veui-tab label="分享朋友圈" name="shares"></veui-tab>
+      </veui-tabs>
+
+      <veui-tabs ui="tiny" :active.sync="active" :index.sync="index0">
+        <veui-tab label="回答问题" name="answers"></veui-tab>
+        <veui-tab label="文章评论" name="articles"></veui-tab>
+        <veui-tab label="分享朋友圈" name="shares"></veui-tab>
       </veui-tabs>
     </section>
     <section>
       <h2>路由模式：</h2>
-      <veui-tabs ui="default" :active.sync="$route.fullPath">
+      <veui-tabs :active.sync="$route.fullPath">
         <veui-tab label="Button" to="/tabs/button"><router-view></router-view></veui-tab>
         <veui-tab label="Input" to="/tabs/input"><router-view></router-view></veui-tab>
         <veui-tab label="Switch" to="/tabs/switch"><router-view></router-view></veui-tab>
+      </veui-tabs>
+    </section>
+    <section>
+      <h2>简单样式：</h2>
+      <p>当前序号 <code>{{index1 + 1}}</code></p>
+      <veui-tabs :index.sync="index1" >
+        <veui-tab label="Tab1">
+          <p>This is Tab1</p>
+        </veui-tab>
+        <veui-tab label="Tab2">
+          <p>This is Tab2</p>
+        </veui-tab>
+        <veui-tab label="Tab3">
+          <p>This is Tab3</p>
+        </veui-tab>
+        <veui-tab label="Tab4">
+          <p>This is Tab4</p>
+        </veui-tab>
       </veui-tabs>
     </section>
     <section>
@@ -28,7 +64,6 @@
         <veui-tab label="Tab3">This is Tab3</veui-tab>
         <veui-tab label="Tab4">This is Tab4</veui-tab>
       </veui-tabs>
-      <!--<div>当前选的 Tab 是：第 {{tabThreeActiveIndex + 1}} 个</div>-->
     </section>
   </article>
 </template>
