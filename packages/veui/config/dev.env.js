e6fe03b2c2b023c148b63c3a72dbb94678f84610
yibuyisheng <yibuyisheng@163.com>
2017-09-07 17:51:08 +0800

    去掉 package 配置

--- packages/veui/config/dev.env.js
+++ packages/veui/config/dev.env.js
@@ -2,6 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  THEME_PACKAGE: '"veui-theme-x"'
+  NODE_ENV: '"development"'
 })
