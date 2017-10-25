e0794ec3ada2c6d5eb1fa7ea80d3f3e864b4fb4d
Justineo <justice360@gmail.com>
2017-10-25 18:15:25 +0800

    add Schedule, fix Tooltip

--- packages/veui/src/utils/date.js
+++ packages/veui/src/utils/date.js
@@ -1,4 +1,5 @@
 import { flatten } from 'lodash'
+import { includes, merge } from './range'
 
 export function getDaysInMonth (year, month) {
   let day
@@ -50,49 +51,13 @@ export function isSameDay (src, target) {
 }
 
 export function isInRange (day, range) {
-  if (!range || range.length < 2) {
-    return false
-  }
-  let date = toDate(day)
-  let dateRange = range.map(toDate)
-  for (let i = 0; i < dateRange.length / 2; i++) {
-    if (date - dateRange[i * 2] >= 0 && dateRange[i * 2 + 1] - date >= 0) {
-      return true
-    }
-  }
-  return false
-}
-
-function compareDate (d1, d2) {
-  return d1 - d2
+  return includes(range.map(toDate), toDate(day))
 }
 
 export function mergeRange (r1, r2) {
-  let dates1 = flatten(r1).sort(compareDate)
-  let dates2 = flatten(r2).sort(compareDate)
-  let dates = [...dates1, ...dates2]
-    .sort(compareDate)
-    .map((date, i) => {
-      if (isInRange(date, dates1) && isInRange(date, dates2)) {
-        return addDays(date, i % 2 ? -1 : 1)
-      }
-      return date
-    })
-
-  for (let i = 0; i < dates.length / 2; i++) {
-    if (dates[i * 2 + 1] - dates[i * 2] < 0) {
-      dates[i * 2 + 1] = dates[i * 2] = null
-    }
-    if (dates[i * 2] - addDays(dates[i * 2 - 1], 1) === 0) {
-      dates[i * 2] = dates[i * 2 - 1] = null
-    }
-  }
-  dates = dates.filter(date => date !== null)
-  let result = []
-  while (dates.length) {
-    result.push(dates.splice(0, 2))
-  }
-  return result
+  r1 = flatten(r1).map(toDate)
+  r2 = flatten(r2).map(toDate)
+  return merge(r1, r2, { inc: addDays })
 }
 
 const ONE_DAY = 24 * 60 * 60 * 1000
---
+++ packages/veui/src/utils/range.js
@@ -0,0 +1,52 @@
+import { flatten } from 'lodash'
+
+export function includes (range, value) {
+  if (!range || range.length < 2) {
+    return false
+  }
+  for (let i = 0; i < range.length / 2; i++) {
+    if (value - range[i * 2] >= 0 && range[i * 2 + 1] - value >= 0) {
+      return true
+    }
+  }
+  return false
+}
+
+const DEFAULT_COMPARE = function (v1, v2) {
+  return v1 - v2
+}
+
+const DEFAULT_INC = function (v1, v2) {
+  return v1 + v2
+}
+
+export function merge (r1, r2, {
+  compare = DEFAULT_COMPARE,
+  inc = DEFAULT_INC
+} = {}) {
+  r1 = flatten(r1).sort(compare)
+  r2 = flatten(r2).sort(compare)
+  let range = [...r1, ...r2]
+    .sort(compare)
+    .map((value, i) => {
+      if (includes(r1, value) && includes(r2, value)) {
+        return inc(value, i % 2 ? -1 : 1)
+      }
+      return value
+    })
+
+  for (let i = 0; i < range.length / 2; i++) {
+    if (range[i * 2 + 1] - range[i * 2] < 0) {
+      range[i * 2 + 1] = range[i * 2] = null
+    }
+    if (range[i * 2] - inc(range[i * 2 - 1], 1) === 0) {
+      range[i * 2] = range[i * 2 - 1] = null
+    }
+  }
+  range = range.filter(date => date !== null)
+  let result = []
+  while (range.length) {
+    result.push(range.splice(0, 2))
+  }
+  return result
+}
