2bf2ebca8568c4e0cb0b8253d5d6d24e999f26e6
Justineo <justice360@gmail.com>
2017-10-29 00:16:42 +0800

    fix default statuses

--- packages/veui-theme-x/components/Schedule.js
+++ packages/veui-theme-x/components/Schedule.js
@@ -2,7 +2,7 @@ import config from 'veui/managers/config'
 
 config.defaults({
   'schedule.statuses': [
-    { slug: 'selected', label: '已选时段' },
-    { slug: 'available', label: '未选时段' }
+    { name: 'selected', label: '已选时段' },
+    { name: 'available', label: '未选时段' }
   ]
 })
