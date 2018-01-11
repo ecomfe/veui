b75adc71432bcb20a98b7f44c194e3b672fb9c2d
Justineo <justice360@gmail.com>
2018-01-11 15:12:30 +0800

    add key-field prop for Table

--- packages/veui/src/components/Table/_TableRow.js
+++ packages/veui/src/components/Table/_TableRow.js
@@ -1,3 +1,4 @@
+import { find } from 'lodash'
 import Checkbox from '../Checkbox'
 import Radio from '../Radio'
 import { table } from '../../mixins'
@@ -22,17 +23,28 @@ export default {
       'selectable',
       'selectMode',
       'selectedItems',
+      'keyField',
       { realKeys: 'keys' }
     )
   },
   render () {
     let index = this.index
-    let checked = !!this.selectedItems[this.keys ? this.keys[index] : index]
     let item = this.data[index]
+    let key = this.keyField
+      ? item[this.keyField]
+      : this.keys[index]
+    let checked = !!this.selectedItems[key]
+
+    let keyCol = find(this.columns, ({ field }) => field === this.keyField)
+    let data = {}
+    if (keyCol) {
+      data = this.getCellSpan(keyCol)
+    }
+
     return <tr class={{ 'veui-table-selected-row': checked }}>
       {
-        this.selectable
-          ? <td><div class="veui-table-cell">
+        this.selectable && data
+          ? <td {...data}><div class="veui-table-cell">
             {
               this.selectMode === 'multiple'
                 ? <veui-checkbox checked={checked}
@@ -45,26 +57,37 @@ export default {
       }
       {
         this.columns.map(col => {
-          let data = {
-            attrs: {}
-          }
-          if (typeof col.span === 'function') {
-            let { col: colspan, row: rowspan } = col.span(index)
-            if (colspan < 1 || rowspan < 1) {
-              return null
-            }
-            if (colspan > 1) {
-              data.attrs.colspan = colspan
-            }
-            if (rowspan > 1) {
-              data.attrs.rowspan = rowspan
-            }
-          }
-          return <td class={col.align ? `veui-table-column-${col.align}` : null} {...data}>
-            <div class="veui-table-cell">{col.renderBody({ ...item, item, index })}</div>
-          </td>
+          let data = this.getCellSpan(col)
+          return data
+            ? <td class={col.align ? `veui-table-column-${col.align}` : null} {...data}>
+              <div class="veui-table-cell">{col.renderBody({ ...item, item, index })}</div>
+            </td>
+            : null
         })
       }
     </tr>
+  },
+  methods: {
+    getCellSpan (col) {
+      let data = {
+        attrs: {}
+      }
+
+      if (typeof col.span === 'function') {
+        let { col: colspan = 1, row: rowspan = 1 } = col.span(this.index)
+        if (colspan < 1 || rowspan < 1) {
+          return null
+        }
+        if (colspan > 1) {
+          data.attrs.colspan = colspan
+        }
+        if (rowspan > 1) {
+          data.attrs.rowspan = rowspan
+        }
+      }
+
+      return data
+    }
   }
 }
+
