5eb0efdb0ae89ffc3b76a6eabfcafc3cfe8273f9
Chestnut Chen <mini.chenli@gmail.com>
2018-01-30 16:12:05 +0800

    删除 maxByte 和 minByte; 校验规则 message 支持传入函数 (#223)

--- packages/veui/src/managers/rules/minByte.js
+++
@@ -1,9 +0,0 @@
-import { isEmpty } from '../../utils/helper'
-
-export default {
-  validate (val, ruleValue) {
-    return !isEmpty(val) ? val.replace(/[^\x00-\xff]/g, 'xxx').length >= ruleValue : true
-  },
-  message: '字节长度不能短于%{ruleValue}',
-  priority: 100
-}
