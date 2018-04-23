f2a470a3ff8e98cd3838bd1242ced4d7bbc28a76
yibuyisheng <yibuyisheng@163.com>
2018-04-23 21:40:09 +0800

    在添加节点的时候就拼装好前后节点信息

--- packages/veui/src/managers/overlay.js
+++ packages/veui/src/managers/overlay.js
@@ -1,4 +1,4 @@
-import { remove, uniqueId, find } from 'lodash'
+import { remove, uniqueId, last, findLast, find } from 'lodash'
 import Vue from 'vue'
 
 /**
@@ -14,20 +14,20 @@ class Node {
   parent = null
 
   /**
-   * 前一个节点
+   * 在前序遍历中的前一个节点
    *
    * @public
    * @type Node
    */
-  previousSibling = null;
+  previousNode = null;
 
   /**
-   * 后一个节点
+   * 在前序遍历中的后一个节点
    *
    * @public
    * @type Node
    */
-  nextSibling = null;
+  nextNode = null;
 
   /**
    * 子节点按照 priority 的分组，结构示例：
@@ -69,18 +69,36 @@ class Node {
   }
 
   /**
+   * 找到第一个 children 不为空的分组
+   *
+   * @public
+   * @param {boolean} [fromLast] 是否倒着找
+   * @param {number} [fromGroupIndex] 从指定索引处开始找
+   * @return {Object}
+   */
+  findFirstNotEmptyGroup (
+    fromLast = false,
+    fromGroupIndex = fromLast ? this.childrenGroup.length - 1 : 0
+  ) {
+    let callback = group => {
+      if (group.children.length) {
+        return true
+      }
+    }
+    return fromLast
+      ? findLast(this.childrenGroup, callback, fromGroupIndex)
+      : find(this.childrenGroup, callback, fromGroupIndex)
+  }
+
+  /**
    * 所有 children 的头部
    *
    * @public
    * @type {Node}
    */
   get head () {
-    for (let i = 0, il = this.childrenGroup.length; i < il; i++) {
-      const group = this.childrenGroup[i]
-      if (group.children.length) {
-        return group.children[0]
-      }
-    }
+    let group = this.findFirstNotEmptyGroup()
+    return group && group.children[0]
   }
 
   /**
@@ -90,62 +108,8 @@ class Node {
    * @type {Node}
    */
   get tail () {
-    for (let i = this.childrenGroup.length - 1; i >= 0; i--) {
-      const group = this.childrenGroup[i]
-      if (group.children.length) {
-        return group.children[group.children.length - 1]
-      }
-    }
-  }
-
-  /**
-   * 修复 child 节点的“前”兄弟节点信息
-   *
-   * @private
-   * @param {Node} child child
-   * @param {number} groupIndex 分组索引
-   * @param {=Array} children 分组下所有的 children
-   */
-  connectToPreviousSibling (child, groupIndex, children) {
-    let previousSibling
-    if (children && children.length > 1) {
-      previousSibling = children[children.length - 2]
-    } else if (groupIndex > 0) {
-      for (let j = groupIndex - 1; j >= 0; j--) {
-        const children = this.childrenGroup[j].children
-        if (children.length) {
-          previousSibling = children[children.length - 1]
-          break
-        }
-      }
-    }
-
-    child.previousSibling = previousSibling
-    if (previousSibling) {
-      previousSibling.nextSibling = child
-    }
-  }
-
-  /**
-   * 修复 child 节点的“后”兄弟节点信息
-   *
-   * @param {Node} child child
-   * @param {number} groupIndex 分组索引
-   */
-  connectToNextSibling (child, groupIndex) {
-    let nextSibling
-    for (let j = groupIndex + 1; j < this.childrenGroup.length; j++) {
-      const children = this.childrenGroup[j].children
-      if (children.length) {
-        nextSibling = children[0]
-        break
-      }
-    }
-
-    child.nextSibling = nextSibling
-    if (nextSibling) {
-      nextSibling.previousSibling = child
-    }
+    let group = this.findFirstNotEmptyGroup(true)
+    return group && last(group.children)
   }
 
   /**
@@ -164,8 +128,6 @@ class Node {
       if (group.priority === priority) {
         group.children.push(child)
         isInserted = true
-        this.connectToPreviousSibling(child, i, group.children)
-        this.connectToNextSibling(child, i)
         childIndex = group.children.length - 1
         groupIndex = i
         return true
@@ -183,15 +145,12 @@ class Node {
           children: [child],
           priority
         })
-        this.connectToPreviousSibling(child, this.childrenGroup.length - 1)
-        groupIndex = 0
+        groupIndex = this.childrenGroup.length - 1
       } else {
         this.childrenGroup.splice(groupIndex, 0, {
           children: [child],
           priority
         })
-        this.connectToNextSibling(child, groupIndex)
-        this.connectToPreviousSibling(child, groupIndex)
       }
     }
 
@@ -208,8 +167,6 @@ class Node {
    * @return {Object} 被移除的节点的索引信息
    */
   removeChildById (id) {
-    let previousBrokenNode
-    let nextBrokenNode
     let groupIndex
     let childIndex
 
@@ -219,34 +176,7 @@ class Node {
       const removedNodes = remove(group.children, (child, index) => {
         if (child.id === id) {
           child.parent = null
-          child.previousSibling = null
-          child.nextSibling = null
-
-          // 找到上一个被破坏掉 nextSibling 的节点
-          if (index > 0) {
-            previousBrokenNode = group.children[index - 1]
-          } else {
-            for (let j = i - 1; j >= 0; j++) {
-              const children = this.childrenGroup[j]
-              if (children.length) {
-                previousBrokenNode = children[children.length - 1]
-                break
-              }
-            }
-          }
-
-          // 找到下一个被破坏掉 previousSibling 的节点
-          if (index < group.children.length - 1) {
-            nextBrokenNode = group.children[index + 1]
-          } else {
-            for (let j = i + 1; j < this.childrenGroup.length; j++) {
-              const children = this.childrenGroup[j]
-              if (children.length) {
-                nextBrokenNode = children[0]
-                break
-              }
-            }
-          }
+          child.previousNode = null
 
           groupIndex = i
           childIndex = index
@@ -258,14 +188,6 @@ class Node {
       if (removedNodes.length === 1) {
         break
       }
-    }
-
-    if (previousBrokenNode) {
-      this.connectToNextSibling(previousBrokenNode, groupIndex)
-    }
-
-    if (nextBrokenNode) {
-      this.connectToPreviousSibling(nextBrokenNode, groupIndex)
     }
 
     return {
@@ -300,18 +222,18 @@ class Node {
   }
 
   /**
-   * 获取指定 child 节点所在的分组
+   * 获取指定 child 节点的索引信息
    *
    * @param {string} targetChildId child id
    * @return {Object}
    */
-  getChildGroup (targetChildId) {
+  getChildIndex (targetChildId) {
     for (let i = 0, il = this.childrenGroup.length; i < il; i++) {
       const group = this.childrenGroup[i]
       for (let j = 0, jl = group.children.length; j < jl; j++) {
         const child = group.children[j]
         if (child.id === targetChildId) {
-          return group
+          return { groupIndex: i, childIndex: j }
         }
       }
     }
@@ -327,8 +249,7 @@ export class Tree {
    *
    * rootNode: {
    *   parent: null,
-   *   nextSibling: null,
-   *   previousSibling: null,
+   *   previousNode: null,
    *   childrenGroup: [
    *     {
    *        priority: 1,
@@ -382,11 +303,87 @@ export class Tree {
    * @param {Function} callback 迭代回调
    */
   iterate ({ curNode = this.rootNode, callback } = {}) {
-    for (let cur = curNode; cur; cur = cur.nextSibling) {
-      if (callback(cur) || cur.head && this.iterate({ curNode: cur.head, callback })) {
-        return true
+    let cur = curNode
+    do {
+      if (callback(cur)) {
+        break
       }
+      cur = cur.nextNode
+    } while (cur)
+  }
+
+  /**
+   * 修复指定节点的 previousNode 和 nextNode 信息
+   *
+   * @private
+   * @param {Node} [node] 待修复的节点
+   * @param {number} [groupIndex] 待修复节点所在的分组，在父节点的 childrenGroup 中的索引位置
+   * @param {number} [childIndex] 待修复节点在自身分组中的索引位置
+   * @param {number} [fixDirection] 修复方向，1 -> 只修复 previous ，2 -> 只修复 next ，3 -> 都修复
+   */
+  fixLink ({ node, groupIndex, childIndex, fixDirection = 3 } = {}) {
+    if (fixDirection === 1 || fixDirection === 3) {
+      let previousNode = this.findPreviousNode(node, groupIndex, childIndex)
+      previousNode.nextNode = node
+      node.previousNode = previousNode
     }
+
+    if (fixDirection === 2 || fixDirection === 3) {
+      let nextNode = this.findNextNode(node, groupIndex, childIndex)
+      if (nextNode) {
+        nextNode.previousNode = node
+      }
+      node.nextNode = nextNode
+    }
+  }
+
+  /**
+   * 包裹一层 Node#appendChild 方法，加上了修复前后节点的功能。
+   *
+   * @private
+   * @param {Node} [parentNode] 父节点
+   * @param {Node} [node] 待插入节点
+   * @param {number} [priority] priority
+   * @return {Object} 插入节点的索引信息
+   */
+  appendChild (parentNode, node, priority) {
+    let { groupIndex, childIndex } = parentNode.appendChild(node, priority)
+    this.fixLink({ node, groupIndex, childIndex })
+
+    let tail = node.tail
+    if (tail) {
+      this.fixLink({
+        node: tail,
+        groupIndex: node.childrenGroup.length - 1,
+        childIndex: last(node.childrenGroup).children.length - 1,
+        fixDirection: 2
+      })
+    }
+
+    return { groupIndex, childIndex }
+  }
+
+  /**
+   * 包一层 Node#remove ，提供清除前后节点信息的功能
+   *
+   * @private
+   * @param {Node} [node] 待移除节点
+   * @return {Object} 索引信息
+   */
+  remove (node) {
+    let previousBrokenNode = node.previousNode
+    let nextBrokenNode = node.nextNode
+
+    let { groupIndex, childIndex } = node.remove()
+
+    if (previousBrokenNode) {
+      this.fixLink({ node: previousBrokenNode, fixDirection: 2 })
+    }
+    if (nextBrokenNode) {
+      this.fixLink({ node: nextBrokenNode, fixDirection: 1 })
+    }
+
+    return { groupIndex, childIndex }
   }
 
   /**
@@ -396,6 +393,7 @@ export class Tree {
    * @param {string} parentId 父节点 id
    * @param {Node} node 待插入子节点
    * @param {number} priority 优先级顺序值
+   * @return {Object} “句柄”
    */
   insertNode (parentId, node, priority) {
     const parent = this.nodeMap[parentId]
@@ -404,13 +402,13 @@ export class Tree {
     }
 
     const parentNode = parent.node
-    parentNode.appendChild(node, priority)
+    this.appendChild(parentNode, node, priority)
 
     this.generateTreeZIndex(node)
   }
 
   /**
-   * 创建一个节点，并将其插入到树种
+   * 创建一个节点，并将其插入到树中
    *
    * @public
    * @param {string} parentId 父节点 id
@@ -436,13 +434,14 @@ export class Tree {
           this.moveNode(node, parentId, priority)
         },
         toTop: () => {
-          const targetGroup = node.parent.getChildGroup(node.id)
+          const { groupIndex } = node.parent.getChildIndex(node.id)
+          const targetGroup = node.parent.childrenGroup[groupIndex]
           const priority = targetGroup.priority
           const parentNode = node.parent
 
-          const previousNode = this.findPreviousNode(node)
-          node.remove()
-          parentNode.appendChild(node, priority)
+          const previousNode = node.previousNode
+          this.remove(node)
+          this.appendChild(parentNode, node, priority)
 
           this.generateTreeZIndex(previousNode)
         }
@@ -473,27 +472,115 @@ export class Tree {
       return
     }
 
-    node.remove()
-    parentNode.appendChild(node, priority)
+    this.remove(node)
+    this.appendChild(parentNode, node, priority)
 
     // 这里不知道是往前移动了，还是往后移动了，不好做判断，干脆整个跑一遍生成 zindex
     this.generateTreeZIndex(this.rootNode)
   }
 
-  findPreviousNode (node) {
-    if (node.previousSibling) {
-      // 找到左兄弟节点的最右侧最深的叶子节点
-      let cur = node.previousSibling
-      let target
-      do {
-        target = cur
-        cur = cur.tail
-      } while (cur)
-      return target || node.previousSibling
+  /**
+   * normalize 一下 index 信息
+   *
+   * @private
+   * @param {Node} node
+   * @param {number=} groupIndex
+   * @param {number=} childIndex
+   * @return {Object}
+   */
+  normalizeIndex (node, groupIndex, childIndex) {
+    if (groupIndex === undefined) {
+      ({ groupIndex, childIndex } = node.parent.getChildIndex(node.id))
     }
 
-    // 左侧没有兄弟节点了，那么父节点就是遍历时候的上一个节点
-    return node.parent
+    let children = node.parent.childrenGroup[groupIndex].children
+    if (childIndex === undefined) {
+      childIndex = children.indexOf(node)
+    }
+
+    return { groupIndex, childIndex }
+  }
+
+  /**
+   * 找到指定节点的前一个节点
+   *
+   * @private
+   * @param {Node} [node] 当前节点
+   * @param {number=} [groupIndex] 当前节点的分组索引
+   * @param {number=} [childIndex] 当前节点在分组中的位置
+   * @return {Node}
+   */
+  findPreviousNode (node, groupIndex, childIndex = -1) {
+    if (node === this.rootNode) {
+      return
+    }
+
+    ({ groupIndex, childIndex } = this.normalizeIndex(node, groupIndex, childIndex))
+
+    let targetNode
+    if (childIndex > 0) {
+      let previousSibling = node.parent.childrenGroup[groupIndex].children[childIndex - 1]
+      let curTail = previousSibling
+      do {
+        targetNode = curTail
+        curTail = curTail.tail
+      } while (curTail)
+
+      return targetNode
+    }
+
+    // 到前面的分组看看
+    if (groupIndex > 0) {
+      findLast(node.parent.childrenGroup.slice(0, groupIndex), (group) => {
+        if (group.children.length) {
+          targetNode = last(group.children)
+          return true
+        }
+      })
+    }
+
+    return targetNode || node.parent
+  }
+
+  /**
+   * 找到指定节点的下一个节点
+   *
+   * @private
+   * @param {Node} [node] 当前节点
+   * @param {number=} [groupIndex] 当前节点的分组索引
+   * @param {number=} [childIndex] 当前节点在分组中的位置
+   * @return {Node}
+   */
+  findNextNode (node, groupIndex, childIndex) {
+    let head = node.head
+    if (head) {
+      return head
+    }
+
+    ({ groupIndex, childIndex } = this.normalizeIndex(node, groupIndex, childIndex))
+
+    let children = node.parent.childrenGroup[groupIndex].children
+    if (childIndex < children.length - 1) {
+      return children[childIndex + 1]
+    }
+
+    let cur = node
+    while (cur.parent) {
+      let { groupIndex, childIndex } = cur.parent.getChildIndex(cur.id)
+      let loopChildren = cur.parent.childrenGroup[groupIndex].children
+      if (childIndex < loopChildren.length - 1) {
+        return loopChildren[childIndex + 1]
+      }
+
+      let group = cur.parent.findFirstNotEmptyGroup(false, groupIndex + 1)
+      if (group && group.children.length) {
+        return group.children[0]
+      }
+
+      cur = cur.parent
+    }
+
+    return null
   }
 
   /**
