25bf7dd13a541d66c41a80360c4f918c6c74dfcc
zhaoyichen <382141176@qq.com>
2018-04-28 16:41:46 +0800

    localValue 默认空字符串 (#301)

--- packages/veui/src/components/Textarea.vue
+++ packages/veui/src/components/Textarea.vue
@@ -61,7 +61,7 @@ export default {
   },
   data () {
     return {
-      localValue: this.value,
+      localValue: this.value || '',
       focused: false,
       height: 0,
       measurerContentWidth: 0,
@@ -108,7 +108,7 @@ export default {
   },
   watch: {
     value (val) {
-      this.localValue = val
+      this.localValue = val || ''
     },
     localValue: {
       handler () {
