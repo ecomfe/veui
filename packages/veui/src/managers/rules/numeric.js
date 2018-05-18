50ff7c274ae6131da22a5e23f2918584c953516b
yibuyisheng <yibuyisheng@163.com>
2018-05-18 12:02:13 +0800

    修复 Table update:selected 事件未抛出的问题；增强 numeric rule 对输入值的校验 (#314)

--- packages/veui/src/managers/rules/numeric.js
+++ packages/veui/src/managers/rules/numeric.js
@@ -1,9 +1,11 @@
-import { isNumber, toNumber, isNaN } from 'lodash'
+import { isNumber, toNumber, isNaN, isString } from 'lodash'
 import { isEmpty } from '../../utils/helper'
 
 export default {
   validate (val) {
-    return !isEmpty(val) ? isNumber(val) || !isNaN(toNumber(val)) : true
+    return !isEmpty(val)
+      ? isNumber(val) || isString(val) && !/^0(?!\.|e)/.test(val) && !isNaN(toNumber(val))
+      : true
   },
   message: '值必须为数字',
   priority: 10
