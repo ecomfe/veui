24239473254f985b24d7751c4d9bdd422acc021d
Chestnut Chen <mini.chenli@gmail.com>
2018-05-21 09:25:52 +0800

    修复 to 是相对路径；修复 tabs 动画怪异问题 (#315)

--- packages/veui/src/components/Tabs/Tab.vue
+++ packages/veui/src/components/Tabs/Tab.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="veui-tab" v-show="isActive" role="tabpanel" :aria-hidden="String(!isActive)">
   <slot v-if="isInited || isActive">
-    <router-view v-if="to && to === $route.fullPath"/>
+    <router-view v-if="to && $route && realTo === $route.fullPath"/>
   </slot>
 </div>
 </template>
@@ -55,6 +55,13 @@ export default {
   computed: {
     isActive () {
       return this.id === this.tabs.activeId
+    },
+    realTo () {
+      if (!this.to) {
+        return null
+      }
+
+      return this.$router ? this.$router.resolve(this.to).route.fullPath : this.to
     }
   },
   created () {
@@ -68,7 +75,7 @@ export default {
 
     this.tabs.add({
       ...pick(this, ...props, 'id'),
-      name: this.to || this.name || this.id,
+      name: this.realTo || this.name || this.id,
       index
     })
 
