2933583ed6702b1b7a2e0b6c70eed2cc98d5b9c2
Justineo <justice360@gmail.com>
2018-08-09 15:09:44 +0800

    refactor: remove moment and use date-fns instead, add babel-preset-veui

---
+++ packages/babel-preset-veui/index.js
@@ -0,0 +1,15 @@
+module.exports = function (context, options) {
+  return {
+    presets: [
+      [
+        require('babel-preset-vue-app'),
+        options
+      ]
+    ],
+    plugins: [
+      'babel-plugin-veui',
+      'babel-plugin-lodash',
+      'babel-plugin-transform-class-properties'
+    ]
+  }
+}
