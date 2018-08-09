39d4036b5e890b4ec4a26e7735216f298b4ea8a5
Justineo <justice360@gmail.com>
2018-08-09 15:45:44 +0800

    test: add cases

---
+++ packages/babel-preset-veui/test/fixtures/expected.js
@@ -0,0 +1,20 @@
+import _classCallCheck from '/Users/justice/dev/veui/packages/babel-preset-veui/node_modules/babel-runtime/helpers/classCallCheck';
+import _extends from '/Users/justice/dev/veui/packages/babel-preset-veui/node_modules/babel-runtime/helpers/extends';
+import Input from 'veui/components/Input.vue';
+
+
+export var a = _extends({ a: 1 });
+
+export var A = function A() {
+  _classCallCheck(this, A);
+
+  this.a = 42;
+};
+
+export default {
+  render: function render() {
+    var h = arguments[0];
+
+    return h('div', ['A']);
+  }
+};
