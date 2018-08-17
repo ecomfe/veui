520f260b645b941e6d53cfdeb6e2fc7c184082c6
Justineo <justice360@gmail.com>
2018-08-17 13:57:16 +0800

    refactor: ui value `top` -> `high` for Dialog, add ui `fliud` for Progress

--- packages/veui/demo/cases/Progress.vue
+++ packages/veui/demo/cases/Progress.vue
@@ -7,12 +7,16 @@
       </p>
       <p>
         <veui-button @click="status = 'alert'">Trigger Error</veui-button>
-        <veui-button @click="status = null">Clear Error</veui-button>
+        <veui-button @click="status = null">Clear Status</veui-button>
       </p>
     </section>
     <section>
       <h2>线性进度条</h2>
       <veui-progress :value="Number(progress)" desc :auto-succeed="200" :status.sync="status" :decimal-place="1"></veui-progress>
+    </section>
+    <section>
+      <h2>线性进度条（自适应宽度）</h2>
+      <veui-progress :value="Number(progress)" ui="fluid" desc :auto-succeed="200" :status.sync="status" :decimal-place="1"></veui-progress>
     </section>
     <section>
       <h2>环形进度条</h2>
