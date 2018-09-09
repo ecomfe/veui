cb45911c0ffa65db3cd705ea1e060c1d9e9e760e
Justineo <justice360@gmail.com>
2018-09-10 01:14:16 +0800

    fix: flex & est config for legacy browsers

--- packages/veui/src/components/Grid/GridRow.vue
+++ packages/veui/src/components/Grid/GridRow.vue
@@ -24,7 +24,7 @@ export default {
       return {
         ...flex
           ? {
-            display: 'flex'
+            display: ['-webkit-box', '-ms-flexbox', 'flex']
           }
           : {},
         'margin-right': `-${gutter / 2}px`,
