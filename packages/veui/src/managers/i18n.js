5c5e84021ec8419d35f9580f31a9805063959530
Justineo <justice360@gmail.com>
2018-10-25 14:30:13 +0800

    feat: reactive config & i18n, remove merge logic in config

--- packages/veui/src/managers/i18n.js
+++ packages/veui/src/managers/i18n.js
@@ -1,58 +1,74 @@
-import { set, get, isString, isFunction, pull } from 'lodash'
+import Vue from 'vue'
+import { isString, isFunction, get } from 'lodash'
+import { deepSet } from '../utils/helper'
 import warn from '../utils/warn'
 
 export class I18nManager {
-  store = {};
+  store = new Vue({
+    data: {
+      locale: process.env.VEUI_LOCALE || 'zh-Hans',
+      store: {}
+    },
+    methods: {
+      register (locale, data, options = {}) {
+        let key = options.ns ? `${locale}.${options.ns}` : `${locale}`
+        deepSet(this.store, key, data)
+      },
+      get (path, data, locale) {
+        if (isString(data)) {
+          // overloading get (path, locale)
+          locale = data
+        }
+
+        let key = `${locale || this.locale}.${path}`
+        let message = get(this.store, key)
+
+        if (!isString(message)) {
+          return message
+        }
+
+        return message.replace(/\{(\w+)\}/g, (_, key) => {
+          return data[key]
+        })
+      },
+      watch (path, callback, locale) {
+        return this.$watch(() => this.get(path, locale), val => {
+          if (isFunction(callback)) {
+            callback(val)
+          }
+        })
+      }
+    }
+  });
 
   _locale = 'zh-Hans';
 
   get locale () {
-    return this._locale
+    return this.store.locale
   }
 
   set locale (val) {
-    if (val === this._locale) {
+    if (val === this.store.locale) {
       return
     }
 
-    if (!this.store[val]) {
+    if (!this.store.store[val]) {
       warn(`[veui-i18n] Locale \`${val}\` not found. Please register locale data before use.`)
     }
 
-    this._locale = val
-    this.deps.forEach(dep => {
-      if (isFunction(dep.$forceUpdate)) {
-        dep.$forceUpdate()
-      }
-    })
+    this.store.locale = val
   }
 
-  deps = []
-
-  register (locale, data, options = {}) {
-    let key = options.ns ? `${locale}.${options.ns}` : `${locale}`
-    set(this.store, key, data)
+  register (locale, data, options) {
+    this.store.register(locale, data, options)
   }
 
-  get (path, data = {}, locale) {
-    let key = `${locale || this.locale}.${path}`
-    let message = get(this.store, key)
-
-    if (!isString(message)) {
-      return message
-    }
-
-    return message.replace(/\{(\w+)\}/g, (_, key) => {
-      return data[key]
-    })
+  get (path, data, locale) {
+    return this.store.get(path, data, locale)
   }
 
-  subscribe (ob) {
-    this.deps.push(ob)
-  }
-
-  unsubscribe (ob) {
-    pull(this.deps, ob)
+  watch (path, callback, locale) {
+    return this.store.watch(path, callback, locale)
   }
 }
 
