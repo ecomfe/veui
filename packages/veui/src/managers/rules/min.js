a24f44dd630f6d39f822e1e234e7eae3a05cbd05
Justineo <justice360@gmail.com>
2018-12-28 18:33:22 +0800

    fix: revert the incorrect isNumber call

--- packages/veui/src/managers/rules/min.js
+++ packages/veui/src/managers/rules/min.js
@@ -1,10 +1,9 @@
-import { isNumber } from 'lodash'
 import { isEmpty } from '../../utils/helper'
 import i18n from '../i18n'
 
 export default {
   validate (val, ruleValue) {
-    return !isEmpty(val) && isNumber(val) ? val >= ruleValue : true
+    return !isEmpty(val) ? val >= ruleValue : true
   },
   message (value, ruleValue) {
     return i18n.get('rules.min', { value, ruleValue })
