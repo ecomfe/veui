aabfc3091f1105cc7e4dde114a279df63a0560ad
Justineo <justice360@gmail.com>
2019-03-11 10:59:40 +0800

    feat: implement `type` prop for Calendar/DatePicker

--- packages/veui/src/locale/en-US/DatePicker.js
+++ packages/veui/src/locale/en-US/DatePicker.js
@@ -5,6 +5,8 @@ i18n.register(
   {
     clear: 'Clear',
     selectDate: 'Select a date',
+    selectMonth: 'Select a month',
+    selectYear: 'Select a year',
     selectRange: 'Select a date range'
   },
   {
