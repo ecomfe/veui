e57d1bf11a413fbf4af465ac28a8602bd8ed31f6
Justineo <justice360@gmail.com>
2019-04-14 20:34:32 +0800

    chore: fix gitignore

--- packages/veui/demo/main.js
+++ packages/veui/demo/main.js
@@ -1,8 +1,10 @@
+import 'classlist-polyfill'
+import 'focus-visible'
+import 'promise-polyfill/src/polyfill'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'classlist-polyfill'
-import 'focus-visible'
 import i18n from 'veui/managers/i18n'
 
 i18n.locale = 'en-US'
