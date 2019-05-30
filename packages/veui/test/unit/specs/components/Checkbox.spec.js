18488587f52c871c410da4b9c463c960c4b42e9b
liuye11 <liuye11@baidu.com>
2019-05-30 17:28:06 +0800

    test: add test case of checkbox component (#448)

--- packages/veui/test/unit/specs/components/Checkbox.spec.js
+++ packages/veui/test/unit/specs/components/Checkbox.spec.js
@@ -7,6 +7,9 @@ describe('components/Checkbox', () => {
       propsData: {
         checked: null
       }
+    },
+    {
+      sync: false
     })
 
     wrapper.vm.$on('change', val => {
@@ -40,8 +43,63 @@ describe('components/Checkbox', () => {
       },
       template:
         '<veui-checkbox v-model="choice" true-value="YES" false-value="NO" @change="handleChange"/>'
+    },
+    {
+      sync: false
     })
 
     wrapper.find('input').trigger('change')
+  })
+
+  it('should support v-model array', async () => {
+    let wrapper = mount(
+      {
+        components: {
+          'veui-checkbox': Checkbox
+        },
+        data () {
+          return {
+            group: ['A']
+          }
+        },
+        template:
+          `<div>
+            <veui-checkbox v-model="group" value="A"> A </veui-checkbox>
+            <veui-checkbox v-model="group" value="B"> B </veui-checkbox>
+            <veui-checkbox v-model="group" value="C"> C </veui-checkbox>
+          </div>`
+      },
+      {
+        sync: false
+      }
+    )
+
+    let boxes = wrapper.findAll('input')
+    boxes.at(0).trigger('change')
+    boxes.at(2).trigger('change')
+
+    let { vm } = wrapper
+    await vm.$nextTick()
+
+    expect(vm.group).to.deep.equal(['A', 'C'])
+
+    wrapper.destroy()
+  })
+
+  it('should handle correctly when activated', () => {
+    let wrapper = mount(Checkbox, {
+      propsData: {
+        checked: false
+      }
+    },
+    {
+      sync: false
+    })
+
+    wrapper.vm.$on('change', val => {
+      expect(val).to.be.equal(true)
+    })
+
+    wrapper.vm.activate()
   })
 })
