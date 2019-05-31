0dafa4c21308001bfc4fe01b057132a9dedc6f89
feibinyang <feibinyang@gmail.com>
2019-05-31 11:49:58 +0800

    test:add test case for ConfirmBox

---
+++ packages/veui/test/unit/specs/components/ConfirmBox.spec.js
@@ -0,0 +1,76 @@
+import { mount } from '@vue/test-utils'
+import sinon from 'sinon'
+import ConfirmBox from '@/components/ConfirmBox'
+
+describe('components/ConfirmBox', function () {
+  it('shoule handle props correctly', async () => {
+    let closeHandler = sinon.spy()
+    let wrapper = mount({
+      components: {
+        'veui-confirm-box': ConfirmBox
+      },
+      data () {
+        return {
+          open: true,
+          closeHandler
+        }
+      },
+      template: '<veui-confirm-box title="title" :open.sync="open" :before-close="closeHandler" />'
+    })
+    let { vm } = wrapper
+    expect(wrapper.find('.veui-dialog-content-head-title').text()).to.be.equal('title')
+    wrapper.find('.veui-button').trigger('click')
+    await vm.$nextTick()
+    expect(closeHandler.callCount).to.be.equal(1)
+    expect(vm.open).to.be.equal(false)
+    wrapper.destroy()
+  })
+
+  it('should render slot correctly', () => {
+    let wrapper = mount(ConfirmBox, {
+      slots: {
+        title: '<div class="title-slot" slot="title">title</div>',
+        default: '<div class="content-slot">content</div>',
+        foot: '<div class="foot-slot" slot="foot">foot</div>'
+      }
+    })
+    expect(wrapper.find('.title-slot').text()).to.be.equal('title')
+    expect(wrapper.find('.content-slot').text()).to.be.equal('content')
+    expect(wrapper.find('.foot-slot').text()).to.be.equal('foot')
+    wrapper.destroy()
+  })
+
+  it('should handle event correctly when click ok/cancel button', async () => {
+    let okHandler = sinon.spy()
+    let cancelHandler = sinon.spy()
+    let wrapper = mount({
+      components: {
+        'veui-confirm-box': ConfirmBox
+      },
+      data () {
+        return {
+          open: true
+        }
+      },
+      methods: {
+        okHandler,
+        cancelHandler
+      },
+      template: `
+        <veui-confirm-box :open.sync="open" title="Confirm" @ok="okHandler" @cancel="cancelHandler">
+          Are you sure you wan to continue?
+        </veui-confirm-box>
+      `
+    })
+    let { vm } = wrapper
+    let buttons = wrapper.findAll('.veui-button')
+    buttons.at(0).trigger('click')
+    await vm.$nextTick()
+    expect(okHandler.callCount).to.be.equal(1)
+    vm.open = true
+    buttons.at(1).trigger('click')
+    await vm.$nextTick()
+    expect(cancelHandler.callCount).to.be.equal(1)
+    wrapper.destroy()
+  })
+})
