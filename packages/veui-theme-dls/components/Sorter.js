1d4df11e0bdf9f8b5e52648ca709b362760b4caa
Justineo <justice360@gmail.com>
2019-06-06 13:02:32 +0800

    feat: fork veui-theme-one to veui-theme-dls

---
+++ packages/veui-theme-dls/components/Sorter.js
@@ -0,0 +1,13 @@
+import 'veui-theme-one-icons/sort-up'
+import 'veui-theme-one-icons/sort-down'
+import config from 'veui/managers/config'
+
+config.defaults(
+  {
+    icons: {
+      asc: 'sort-up',
+      desc: 'sort-down'
+    }
+  },
+  'sorter'
+)
---
+++ packages/veui-theme-dls/components/Uploader.js
@@ -0,0 +1,38 @@
+import 'veui-theme-one-icons/upload'
+import 'veui-theme-one-icons/times'
+import 'veui-theme-one-icons/check'
+import 'veui-theme-one-icons/paperclip'
+import 'veui-theme-one-icons/sync-alt'
+import 'veui-theme-one-icons/plus'
+import 'veui-theme-one-icons/exclamation-circle'
+import config from 'veui/managers/config'
+
+config.defaults(
+  {
+    icons: {
+      upload: 'upload',
+      clear: 'times',
+      success: 'check',
+      redo: 'sync-alt',
+      file: 'paperclip',
+      add: 'plus',
+      alert: 'exclamation-circle'
+    },
+    ui: {
+      direction: {
+        values: ['horizontal']
+      }
+    },
+    parts: {
+      retryFileDone: 'link micro',
+      clearFileDone: 'link square micro',
+      cancelFile: 'link square micro',
+      cancelImage: 'small',
+      retryImageSuccess: 'dark',
+      clearImageSuccess: 'dark square micro',
+      retryImageFailure: 'small',
+      clearImageFailure: 'link square micro'
+    }
+  },
+  'uploader'
+)
