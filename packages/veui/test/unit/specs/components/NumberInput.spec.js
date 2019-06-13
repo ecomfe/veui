b8d9906779f89b2cb522dd546d609a1f02be954a
7inspire <tongchangsheng@baidu.com>
2019-06-13 15:39:25 +0800

    add test case for NumberInput

--- packages/veui/test/unit/specs/components/NumberInput.spec.js
+++ packages/veui/test/unit/specs/components/NumberInput.spec.js
@@ -30,4 +30,70 @@ describe('components/NumberInput', () => {
     expect(wrapper.find(Input).props('selectOnFocus')).to.equal(true)
     expect(wrapper.find('input').element.autofocus).to.equal(true)
   })
+
+  it('should hanlde focus event correctly', async () => {
+    let wrapper = mount(NumberInput)
+    wrapper.find('input').trigger('focus')
+
+    let { vm } = wrapper
+    await vm.$nextTick()
+
+    expect(
+      wrapper.find('div.veui-input').classes('veui-input-focused')
+    ).to.equal(true)
+
+    wrapper.destroy()
+  })
+
+  it('disabled input should not be focused when activated', async () => {
+    let wrapper = mount(NumberInput, {
+      propsData: {
+        disabled: true
+      }
+    })
+
+    let { vm } = wrapper
+    vm.activate()
+    await vm.$nextTick()
+
+    expect(
+      wrapper.find('div.veui-input').classes('veui-input-focused')
+    ).to.equal(false)
+
+    wrapper.destroy()
+  })
+
+  it('should not exceed max or min value', async () => {
+    let wrapper = mount(NumberInput, {
+      propsData: {
+        max: 2,
+        min: 2
+      }
+    })
+
+    let input = wrapper.find('input')
+    input.setValue(2)
+    wrapper.find('button.veui-number-input-step-up').trigger('click')
+    expect(input.element.value).to.equal('2')
+
+    input.setValue(2)
+    wrapper.find('button.veui-number-input-step-down').trigger('click')
+    expect(input.element.value).to.equal('2')
+  })
+
+  it('should handle readonly correctly', async () => {
+    let wrapper = mount(NumberInput, {
+      propsData: {
+        readonly: true
+      }
+    })
+
+    let input = wrapper.find('input.veui-input-input')
+    input.setValue(2)
+    expect(
+      input.attributes('readonly')
+    ).to.equal('readonly')
+
+    expect(input.element.value).to.equal('2')
+  })
 })
