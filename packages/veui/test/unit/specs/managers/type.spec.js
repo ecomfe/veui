a72c3432a47df23e5ee7e0041cdae9f18060cfbc
feibinyang <feibinyang@gmail.com>
2019-06-13 17:18:02 +0800

    test:add test case for managers/type

--- packages/veui/test/unit/specs/managers/type.spec.js
+++ packages/veui/test/unit/specs/managers/type.spec.js
@@ -1,57 +1,56 @@
-import type from '@/managers/type'
-import { cloneDeep } from 'lodash'
+import { Type } from '@/managers/type'
 
 describe('managers/type', () => {
   it('should init `type` instance correctly', () => {
-    let curType = cloneDeep(type)
+    let type = new Type()
 
-    expect(curType.configs.length).to.equal(1)
-    expect(curType.configs[0].type).to.equal(Function.prototype.constructor)
-    expect(curType.configs[0].clone({ a: 1 })).to.deep.equal({ a: 1 })
+    expect(type.configs.length).to.equal(1)
+    expect(type.configs[0].type).to.equal(Function.prototype.constructor)
+    expect(type.configs[0].clone({ a: 1 })).to.deep.equal({ a: 1 })
   })
 
   it('should implement `register` function correctly', () => {
-    let curType = cloneDeep(type)
-    curType.register(null)
+    let type = new Type()
+    type.register(null)
 
-    expect(curType.configs.length).to.equal(1)
+    expect(type.configs.length).to.equal(1)
 
-    curType.register({
+    type.register({
       type: Array,
       clone (value) {
         return [].concat(value)
       }
     })
 
-    expect(curType.configs.length).to.equal(2)
-    expect(curType.configs[1].type).to.equal(Array.prototype.constructor)
-    expect(curType.configs[1].clone({ a: 1 })).to.deep.equal([{ a: 1 }])
+    expect(type.configs.length).to.equal(2)
+    expect(type.configs[1].type).to.equal(Array.prototype.constructor)
+    expect(type.configs[1].clone({ a: 1 })).to.deep.equal([{ a: 1 }])
 
-    curType.register({
+    type.register({
       type: Array,
       clone (value) {
         return value
       }
     })
 
-    expect(curType.configs.length).to.equal(2)
-    expect(curType.configs[1].type).to.equal(Array.prototype.constructor)
-    expect(curType.configs[1].clone({ a: 1 })).to.deep.equal({ a: 1 })
+    expect(type.configs.length).to.equal(2)
+    expect(type.configs[1].type).to.equal(Array.prototype.constructor)
+    expect(type.configs[1].clone({ a: 1 })).to.deep.equal({ a: 1 })
   })
 
   it('should implement `clone` function correctly', () => {
-    let curType = cloneDeep(type)
+    let type = new Type()
 
-    expect(curType.clone({ a: 1 })).to.deep.equal({ a: 1 })
-    expect(curType.clone(1)).to.equal(1)
+    expect(type.clone({ a: 1 })).to.deep.equal({ a: 1 })
+    expect(type.clone(1)).to.equal(1)
 
-    curType.register({
+    type.register({
       type: Number,
       clone (value) {
         return value + 1
       }
     })
 
-    expect(curType.clone(1)).to.equal(2)
+    expect(type.clone(1)).to.equal(2)
   })
 })
