494357cf91fa4e02e2b92ec0d118cdea2b72c19c
feibinyang <feibinyang@gmail.com>
2019-06-13 10:58:13 +0800

    test:add test case for managers/type

---
+++ packages/veui/test/unit/specs/managers/type.spec.js
@@ -0,0 +1,57 @@
+import type from '@/managers/type'
+import { cloneDeep } from 'lodash'
+
+describe('managers/type', () => {
+  it('should init `type` instance correctly', () => {
+    let curType = cloneDeep(type)
+
+    expect(curType.configs.length).to.equal(1)
+    expect(curType.configs[0].type).to.equal(Function.prototype.constructor)
+    expect(curType.configs[0].clone({ a: 1 })).to.deep.equal({ a: 1 })
+  })
+
+  it('should realize `register` function correctly', () => {
+    let curType = cloneDeep(type)
+    curType.register(null)
+
+    expect(curType.configs.length).to.equal(1)
+
+    curType.register({
+      type: Array,
+      clone (value) {
+        return [].concat(value)
+      }
+    })
+
+    expect(curType.configs.length).to.equal(2)
+    expect(curType.configs[1].type).to.equal(Array.prototype.constructor)
+    expect(curType.configs[1].clone({ a: 1 })).to.deep.equal([{ a: 1 }])
+
+    curType.register({
+      type: Array,
+      clone (value) {
+        return value
+      }
+    })
+
+    expect(curType.configs.length).to.equal(2)
+    expect(curType.configs[1].type).to.equal(Array.prototype.constructor)
+    expect(curType.configs[1].clone({ a: 1 })).to.deep.equal({ a: 1 })
+  })
+
+  it('should realize `clone` function correctly', () => {
+    let curType = cloneDeep(type)
+
+    expect(curType.clone({ a: 1 })).to.deep.equal({ a: 1 })
+    expect(curType.clone(1)).to.equal(1)
+
+    curType.register({
+      type: Number,
+      clone (value) {
+        return value + 1
+      }
+    })
+
+    expect(curType.clone(1)).to.equal(2)
+  })
+})
