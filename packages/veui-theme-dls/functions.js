b66ac5c161c82d6c1e8b2821d8b8eb45a7bfae87
Justineo <justice360@gmail.com>
2019-06-16 15:59:02 +0800

    feat: switch Radio to DLS

--- packages/veui-theme-dls/functions.js
+++ packages/veui-theme-dls/functions.js
@@ -1,6 +1,9 @@
 module.exports = {
   install (less, pluginManager, functions) {
     functions.add('args', node => {
+      if (typeof node.value === 'string') {
+        return node.value
+      }
       let result = []
       for (let i = 0; i < node.value.length; i++) {
         result.push(node.value[i].value)
