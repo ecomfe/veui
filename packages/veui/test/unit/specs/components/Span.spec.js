0fff760ec776643e5cacbf4bd9f4456d1f7f23fc
StarShinny <877106705@qq.com>
2019-06-20 15:53:35 +0800

    test: add test cases for Span (#493)

---
+++ packages/veui/test/unit/specs/components/Span.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+import Span from '@/components/Span'
+
+describe('components/Span', () => {
+  it('should support customized ui correctly.', async () => {
+    let wrapper = mount(Span, {
+      sync: false,
+      propsData: {
+        ui: 'large'
+      }
+    })
+
+    expect(wrapper.attributes('ui')).to.equal('large')
+
+    wrapper.setProps({ ui: 'micro' })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.attributes('ui')).to.equal('micro')
+
+    wrapper.destroy()
+  })
+
+  it('should render default slot correctly.', () => {
+    let wrapper = mount(Span, {
+      sync: false,
+      slots: {
+        default: '<a class="link" href="#">Link</a>'
+      }
+    })
+
+    expect(wrapper.contains('a.link')).to.equal(true)
+    wrapper.destroy()
+  })
+})
