7a42d9ceca248e916e324f185fd369db7d15d11e
r7r10t32 <382141176@baidu.com>
2019-06-20 14:41:37 +0800

    fix: fix tooltip pass wrong refs to outside

--- packages/veui/src/directives/outside.js
+++ packages/veui/src/directives/outside.js
@@ -55,7 +55,6 @@ function initBindingType (type) {
 
 function getElementsByRefs (refs, context) {
   const elements = []
-  refs = Array.isArray(refs) ? refs : [refs]
   refs.forEach(ref => {
     elements.push(...getNodes(ref, context))
   })
