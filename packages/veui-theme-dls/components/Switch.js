cf4a353cb13025488fba46fd574c749069986a4a
Justineo <justice360@gmail.com>
2019-06-28 12:01:14 +0800

    feat: add loading state for Switch

--- packages/veui-theme-dls/components/Switch.js
+++ packages/veui-theme-dls/components/Switch.js
@@ -1,10 +1,14 @@
+import 'veui-theme-one-icons/spinner'
 import config from 'veui/managers/config'
 
 config.defaults(
   {
+    icons: {
+      loading: 'spinner'
+    },
     ui: {
       size: {
-        values: ['large', 'small']
+        values: ['xs', 's', 'm']
       }
     }
   },
