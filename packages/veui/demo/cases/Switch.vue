72aa463a661abf7658a394b5545f270051fd32f5
Justineo <justice360@gmail.com>
2019-06-28 12:01:14 +0800

    feat: add loading state for Switch

--- packages/veui/demo/cases/Switch.vue
+++ packages/veui/demo/cases/Switch.vue
@@ -2,25 +2,53 @@
 <article>
   <h1><code>&lt;veui-switch&gt;</code></h1>
   <section>
+    <veui-switch v-model="loading">
+      加载中
+    </veui-switch>
+  </section>
+  <section>
     <veui-switch
       v-model="value1"
-      ui="small"
+      ui="xs"
+      :loading="loading"
     >
       夜间模式
     </veui-switch>
     <veui-switch
       v-model="value1"
-      ui="small"
+      ui="xs"
       readonly
     />
     <veui-switch
       v-model="value1"
-      ui="small"
+      ui="xs"
       disabled
     />
   </section>
   <section>
-    <veui-switch v-model="value2">
+    <veui-switch
+      v-model="value3"
+      ui="s"
+      :loading="loading"
+    >
+      飞行模式
+    </veui-switch>
+    <veui-switch
+      v-model="value3"
+      ui="s"
+      readonly
+    />
+    <veui-switch
+      v-model="value3"
+      ui="s"
+      disabled
+    />
+  </section>
+  <section>
+    <veui-switch
+      v-model="value2"
+      :loading="loading"
+    >
       勿扰模式
     </veui-switch>
     <veui-switch
@@ -34,21 +62,10 @@
   </section>
   <section>
     <veui-switch
-      v-model="value3"
-      ui="large"
-    >
-      飞行模式
-    </veui-switch>
-    <veui-switch
-      v-model="value3"
-      ui="large"
-      readonly
-    />
-    <veui-switch
-      v-model="value3"
-      ui="large"
-      disabled
-    />
+      :checked="asyncValue"
+      :loading="asyncLoading"
+      @click.prevent="toggle"
+    >异步</veui-switch>
   </section>
 </article>
 </template>
@@ -65,7 +82,19 @@ export default {
     return {
       value1: true,
       value2: false,
-      value3: true
+      value3: true,
+      loading: false,
+      asyncValue: false,
+      asyncLoading: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.asyncLoading = true
+      setTimeout(() => {
+        this.asyncValue = !this.asyncValue
+        this.asyncLoading = false
+      }, 2000)
     }
   }
 }
