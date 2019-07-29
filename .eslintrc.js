2cd9558343bf3508305f3befd17f9af9efba7de1
Justineo <justice360@gmail.com>
2019-07-29 19:59:53 +0800

    style: code style fix for quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    quotes: ['error', 'single'],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
