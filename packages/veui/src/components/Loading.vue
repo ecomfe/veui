6a93d7161ef9c9b20a3e32f2c21a5ae001b3d104
fengchongxiu <1028669058@qq.com>
2019-07-29 17:29:31 +0800

    feat: add loading component

---
+++ packages/veui/src/components/Loading.vue
@@ -0,0 +1,41 @@
+<template>
+<div
+  v-if="loading"
+  class="veui-loading"
+>
+  <veui-span
+    class="veui-loading-spinner"
+    :ui="realUi"
+  >
+    <slot name="spinner">
+      <veui-icon
+        spin
+        :name="icons.loading"
+      />
+    </slot>
+  </veui-span>
+  <veui-span
+    :ui="realUi"
+    class="veui-loading-text"
+  >
+    <slot/>
+  </veui-span>
+</div>
+</template>
+<script>
+import Icon from './Icon'
+import Span from './Span'
+import ui from '../mixins/ui'
+
+export default {
+  name: 'veui-loading',
+  components: {
+    'veui-icon': Icon,
+    'veui-span': Span
+  },
+  mixins: [ui],
+  props: {
+    loading: Boolean
+  }
+}
+</script>
