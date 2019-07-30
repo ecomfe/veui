cf58f5a1dfd607b73e530725c35fdb573931d45d
fengchongxiu <1028669058@qq.com>
2019-07-30 21:10:11 +0800

    fix: code style fix

--- packages/veui/src/components/Loading.vue
+++ packages/veui/src/components/Loading.vue
@@ -1,11 +1,12 @@
 <template>
 <div
   v-if="loading"
+  :ui="realUi"
   class="veui-loading"
 >
-  <veui-span
-    class="veui-loading-spinner"
+  <div
     :ui="realUi"
+    class="veui-loading-spinner"
   >
     <slot name="spinner">
       <veui-icon
@@ -13,25 +14,24 @@
         :name="icons.loading"
       />
     </slot>
-  </veui-span>
-  <veui-span
+  </div>
+  <div
+    v-if="$slots.default"
     :ui="realUi"
     class="veui-loading-text"
   >
     <slot/>
-  </veui-span>
+  </div>
 </div>
 </template>
 <script>
 import Icon from './Icon'
-import Span from './Span'
 import ui from '../mixins/ui'
 
 export default {
   name: 'veui-loading',
   components: {
-    'veui-icon': Icon,
-    'veui-span': Span
+    'veui-icon': Icon
   },
   mixins: [ui],
   props: {
