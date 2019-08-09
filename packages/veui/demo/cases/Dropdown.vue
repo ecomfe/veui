5ca98d6efab05aef183b43288d1aa66196823e56
fengchongxiu <1028669058@qq.com>
2019-08-09 16:20:26 +0800

    feat: switch Dropdown to DLS

--- packages/veui/demo/cases/Dropdown.vue
+++ packages/veui/demo/cases/Dropdown.vue
@@ -1,77 +1,248 @@
 <template>
 <article>
   <h1><code>&lt;veui-dropdown&gt;</code></h1>
-  <p>
-    <veui-dropdown
-      ui="primary"
-      label="操作"
-      split
-      :options="options"
-    />
-    <veui-dropdown
-      ui="primary micro"
-      label="操作操作操作操作操作操作"
-      trigger="hover"
-      :options="options"
-    >
-      <template
-        slot="option-label"
-        slot-scope="{ label }"
+  <div>
+    <h3>基础下拉菜单</h3>
+    <div>
+      <h4>primary</h4>
+      <veui-dropdown
+        ui="primary xs"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="primary s"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="primary m"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="primary l"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="primary"
+        label="操作"
+        split
+        disabled
+        :options="options"
+      />
+    </div>
+    <div>
+      <h4>normal</h4>
+      <veui-dropdown
+        ui="xs"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="s"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="m"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        ui="l"
+        label="操作"
+        split
+        :options="options"
+      />
+      <veui-dropdown
+        label="操作"
+        split
+        disabled
+        :options="options"
+      />
+    </div>
+    <div>
+      <h4>disabled</h4>
+      <div>
+        <veui-dropdown
+          ui="s"
+          label="操作操作操作操作操作操作"
+          trigger="hover"
+          :options="options"
+        />
+        <veui-dropdown
+          ui="s"
+          label="操作操作操作操作操作操作"
+          disabled
+          :options="options"
+        />
+        <veui-dropdown
+          split
+          ui="s"
+          label="操作操作操作操作操作操作"
+          trigger="hover"
+          :options="options"
+        />
+        <veui-dropdown
+          split
+          disabled
+          ui="s"
+          label="操作操作操作操作操作操作"
+          trigger="hover"
+          :options="options"
+        />
+      </div>
+    </div>
+    <div>
+      <h4>triggers</h4>
+      <div>
+        <veui-dropdown
+          ui="xs"
+          label="操作"
+          split
+          :options="options"
+        />
+        <veui-dropdown
+          ui="xs"
+          split
+          label="操作操作操作操作操作操作"
+          trigger="hover"
+          :options="options"
+        >
+          <template
+            slot="option-label"
+            slot-scope="{ label }"
+          >
+            👉 {{ label }}
+          </template>
+        </veui-dropdown>
+      </div>
+    </div>
+  </div>
+  <div>
+    <h3>分组下拉框菜单</h3>
+    <div>
+      <veui-dropdown
+        ui="xs"
+        split
+        label="汽车类型"
+        :options="multiLevelOptions"
+      />
+      <veui-dropdown
+        ui="s"
+        split
+        label="汽车类型"
+        :options="multiLevelOptions"
+      />
+      <veui-dropdown
+        ui="m"
+        split
+        label="汽车类型"
+        :options="multiLevelOptions"
+      />
+      <veui-dropdown
+        ui="l"
+        split
+        label="汽车类型"
+        :options="multiLevelOptions"
+      />
+    </div>
+    <div>
+      <h4>unlabeled group</h4>
+      <veui-dropdown
+        ui="xs"
+        split
+        label="汽车类型"
+        :options="multiLevelUnlabeledOptions"
+      />
+      <veui-dropdown
+        ui="s"
+        split
+        label="汽车类型"
+        :options="multiLevelUnlabeledOptions"
+      />
+      <veui-dropdown
+        ui="m"
+        split
+        label="汽车类型"
+        :options="multiLevelUnlabeledOptions"
+      />
+      <veui-dropdown
+        ui="l"
+        split
+        label="汽车类型"
+        :options="multiLevelUnlabeledOptions"
+      />
+    </div>
+  </div>
+  <div>
+    <h3>多层级下拉框菜单</h3>
+    <div>
+      <veui-dropdown
+        ui="s"
+        split
+        label="汽车类型"
+        trigger="hover"
+        :options="multiLevelUnlabeledOptions"
+      />
+      <veui-dropdown
+        ui="s"
+        split
+        label="汽车类型"
+        trigger="click"
+        :options="multiLevelUnlabeledOptions"
+      />
+    </div>
+  </div>
+  <div>
+    <h3>文字链下拉框菜单</h3>
+    <div>
+      <veui-dropdown
+        ui="text"
+        label="操作"
+        disabled
+        :options="options"
+      />
+      <veui-dropdown
+        ui="text m"
+        label="操作"
       >
-        👉 {{ label }}
-      </template>
-    </veui-dropdown>
-  </p>
-  <p>
-    <veui-dropdown
-      ui="small"
-      label="操作操作操作操作操作操作"
-      trigger="hover"
-      split
-      :options="options"
-    />
-    <veui-dropdown
-      ui="primary"
-      label="操作操作操作操作操作操作"
-      disabled
-      :options="options"
-    />
-  </p>
-  <p>
-    <veui-dropdown
-      ui="link"
-      label="操作"
-    >
-      <veui-option-group label="提交">
-        <veui-option
-          label="保存"
-          @click="log('save')"
-        />
-        <veui-option
-          value="publish"
-          label="发布"
-        />
-      </veui-option-group>
-      <veui-option-group label="操作">
-        <veui-option
-          value="undo"
-          label="撤销"
-        />
-        <veui-option
-          value="redo"
-          label="重复"
-        />
-      </veui-option-group>
-    </veui-dropdown>
-  </p>
-  <p>
-    <veui-dropdown
-      ui="link tiny"
-      label="操作"
-      disabled
-      :options="options"
-    />
-  </p>
+        <veui-option-group label="提交">
+          <veui-option
+            label="保存"
+            @click="log('save')"
+          />
+          <veui-option
+            value="publish"
+            label="发布"
+          />
+        </veui-option-group>
+        <veui-option-group label="操作">
+          <veui-option
+            value="undo"
+            label="撤销"
+          />
+          <veui-option
+            value="redo"
+            label="重复"
+          />
+        </veui-option-group>
+      </veui-dropdown>
+    </div>
+  </div>
+  <div>
+    <h3>含搜索的下拉框菜单</h3>
+    待补充
+  </div>
 </article>
 </template>
 
@@ -101,6 +272,100 @@ export default {
         {
           label: '删除',
           value: 'remove'
+        },
+        {
+          label: '编辑',
+          value: 'edit',
+          disabled: true
+        },
+        {
+          label: '删除',
+          value: 'remove'
+        },
+        {
+          label: '编辑',
+          value: 'edit',
+          disabled: true
+        },
+        {
+          label: '删除',
+          value: 'remove'
+        },
+        {
+          label: '编辑',
+          value: 'edit',
+          disabled: true
+        },
+        {
+          label: '删除',
+          value: 'remove'
+        },
+        {
+          label: '编辑',
+          value: 'edit',
+          disabled: true
+        },
+        {
+          label: '删除',
+          value: 'remove'
+        }
+      ],
+      multiLevelOptions: [
+        {
+          label: '丰田',
+          options: [
+            {
+              label: '凯美瑞',
+              value: 'camry'
+            },
+            {
+              label: '亚洲龙',
+              value: 'avalon'
+            }
+          ]
+        },
+        {
+          label: '大众',
+          options: [
+            {
+              label: '迈腾',
+              value: 'magotan'
+            },
+            {
+              label: '高尔夫',
+              value: 'golf'
+            }
+          ]
+        }
+      ],
+      multiLevelUnlabeledOptions: [
+        {
+          label: '丰田',
+          position: 'popup',
+          options: [
+            {
+              label: '凯美瑞',
+              value: 'camry'
+            },
+            {
+              label: '亚洲龙',
+              value: 'avalon'
+            }
+          ]
+        },
+        {
+          label: '大众',
+          position: 'popup',
+          options: [
+            {
+              label: '迈腾',
+              value: 'magotan'
+            },
+            {
+              label: '高尔夫',
+              value: 'golf'
+            }
+          ]
         }
       ]
     }
--- packages/veui/test/unit/specs/components/Select/OptionGroup.spec.js
+++ packages/veui/test/unit/specs/components/Select/OptionGroup.spec.js
@@ -1,4 +1,5 @@
 import Select from 'veui/components/Select'
+import Dropdown from 'veui/components/Dropdown'
 import Vue from 'vue'
 import { some } from 'lodash'
 import { mount } from '@vue/test-utils'
@@ -84,55 +85,100 @@ describe('components/Select/OptionGroup', () => {
   })
 
   it('should not open subMenu if disabled prop of OptionGroup is true', async () => {
-    let wrapper = mount({
-      components: {
-        'veui-select': Select
+    let wrapper = mount(
+      {
+        components: {
+          'veui-select': Select
+        },
+        data () {
+          return {
+            options: [
+              {
+                label: 'a',
+                position: 'popup',
+                options: [
+                  {
+                    label: 'e',
+                    value: 'e'
+                  }
+                ]
+              },
+              {
+                label: 'b',
+                disabled: true,
+                position: 'popup',
+                options: [
+                  {
+                    label: 'c',
+                    value: 'c'
+                  },
+                  {
+                    label: 'd',
+                    value: 'd'
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        template: '<veui-select :options="options"/>'
       },
-      data () {
-        return {
-          options: [
-            {
-              label: 'a',
-              position: 'popup',
-              options: [
-                {
-                  label: 'e',
-                  value: 'e'
-                }
-              ]
-            },
-            {
-              label: 'b',
-              disabled: true,
-              position: 'popup',
-              options: [
-                {
-                  label: 'c',
-                  value: 'c'
-                },
-                {
-                  label: 'd',
-                  value: 'd'
-                }
-              ]
-            }
-          ]
-        }
-      },
-      template: '<veui-select :options="options"/>'
-    })
+      { sync: false }
+    )
     let controlButton = wrapper.find('.veui-select-input .veui-input-main')
     controlButton.trigger('click')
     await wrapper.vm.$nextTick()
     let options = wrapper.findAll('.veui-option')
     let command = wrapper.findAll('.veui-option-group-button')
-    let menus = wrapper.findAll('.veui-option-group-box')
     expect(options.length).to.equal(3)
     command.wrappers[1].trigger('click')
-
+    await wrapper.vm.$nextTick()
+    let menus = wrapper.findAll('.veui-option-group-box')
     expect(menus.wrappers[1].element.style.display).to.equal('none')
     command.wrappers[0].trigger('click')
+    await wrapper.vm.$nextTick()
     expect(menus.wrappers[0].element.style.display).to.equal('')
+    wrapper.destroy()
+  })
+
+  it('should handle trigger prop correctly', async () => {
+    let wrapper = mount(
+      {
+        components: {
+          'veui-dropdown': Dropdown
+        },
+        data () {
+          return {
+            options: [
+              {
+                label: 'h',
+                position: 'popup',
+                options: [
+                  {
+                    label: 'm',
+                    value: 'm'
+                  },
+                  {
+                    label: 'n',
+                    value: 'n'
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        template: '<veui-dropdown :options="options" trigger="hover"/>'
+      },
+      { sync: false }
+    )
+    let btn = wrapper.find('.veui-dropdown-button')
+    btn.trigger('mouseenter')
+    await wrapper.vm.$nextTick()
+    let label = wrapper.find('.veui-option-group-button')
+    label.trigger('mouseenter')
+    await wrapper.vm.$nextTick()
+    let group = wrapper.find('.veui-option-group-box')
+    expect(group.element.style.display).to.equal('')
     wrapper.destroy()
   })
 })
