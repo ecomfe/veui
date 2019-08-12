559baba7fcf71b21685fa97bc5fb5b977a0aa5bc
xiaodemen <fuq_pr@163.com>
2019-08-12 20:44:03 +0800

    feat: add TimePicker

---
+++ packages/veui/src/components/TimePicker/_TimePickerOptionGroup.vue
@@ -0,0 +1,35 @@
+<script>
+import { omit } from 'lodash'
+import OptionGroup from '../OptionGroup'
+
+export default {
+  name: 'veui-time-picker-option-group',
+  uiTypes: ['select'],
+  inheritAttrs: false,
+  props: {
+    value: Number
+  },
+  methods: {
+    handleSelect (value) {
+      this.$emit('update:value', value)
+    }
+  },
+  render (h) {
+    let data = {
+      // 一定要 spread，否则响应式有问题
+      attrs: {
+        ...this.$attrs
+      },
+      on: omit(this.$listeners, 'update:value'),
+      scopedSlots: this.$scopedSlots
+    }
+    return h(
+      OptionGroup,
+      data,
+      Object.keys(this.$slots).map(slot =>
+        h('template', { slot }, this.$slots[slot])
+      )
+    )
+  }
+}
+</script>
