24b35f171cfd3142c4d58f9b0f22489a5e939b27
Hank Liang <ly604547351@163.com>
2019-08-15 21:20:13 +0800

    feat: switch Toast & Alert to DLS (#531)

--- packages/veui/demo/cases/Alert.vue
+++ packages/veui/demo/cases/Alert.vue
@@ -5,18 +5,62 @@
     <div>固定提示</div>
     <veui-alert
       type="success"
+      ui="strong"
       message="恭喜你，你的请求已成功处理"
       closable
-    />
+    >
+      <template slot="title">
+        恭喜你
+      </template>
+      <template slot="extra">
+        <veui-button ui="text">
+          查看详情
+        </veui-button>
+      </template>
+      恭喜你，你的请求已成功处理
+    </veui-alert>
     <veui-alert
-      class="limit-width"
       type="success"
       message="恭喜你，你的请求已成功处理"
       closable
-      close-label="关闭"
+      :show-icon="false"
+    >
+      <template slot="title">
+        恭喜你
+      </template>
+      <template slot="extra">
+        <veui-button ui="text">
+          查看详情
+        </veui-button>
+      </template>
+      恭喜你，你的请求已成功处理
+    </veui-alert>
+    <veui-alert
+      type="success"
+      message="恭喜你，你的请求已成功处理"
+      closable
+      ui="m strong"
+    >
+      <template slot="title">
+        恭喜你
+      </template>
+      <template slot="extra">
+        <veui-button ui="text">
+          查看详情
+        </veui-button>
+      </template>
+      恭喜你，你的请求已成功处理
+    </veui-alert>
+    <veui-alert
+      class="limit-width"
+      ui="strong"
+      type="success"
+      message="恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理 恭喜你，你的请求已成功处理"
+      closable
     />
     <veui-alert
       type="warning"
+      ui="strong"
       message="警告，进行检查，有风险信息存在"
     />
     <veui-alert
@@ -25,6 +69,19 @@
       :message="messages"
       closable
     />
+    <veui-alert
+      class="limit-width"
+      type="warning"
+      :message="messages"
+      closable
+    >
+      <template
+        slot="extra"
+        slot-scope="{ message }"
+      >
+        <veui-button ui="text"> 查看详情 {{ message }} </veui-button>
+      </template>
+    </veui-alert>
     <veui-alert
       type="info"
       message="提醒，这个消息需要注意"
@@ -50,12 +107,13 @@
 
 <script>
 import bus from '../bus'
-import { Alert } from 'veui'
+import { Alert, Button } from 'veui'
 
 export default {
   name: 'alert',
   components: {
-    'veui-alert': Alert
+    'veui-alert': Alert,
+    'veui-button': Button
   },
   data () {
     return {
@@ -95,6 +153,9 @@ section {
 }
 
 .veui-alert.limit-width {
-  width: 350px;
+  width: 650px;
+}
+.veui-button {
+  font-size: 12px;
 }
 </style>
