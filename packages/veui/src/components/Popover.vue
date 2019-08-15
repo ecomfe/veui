3678578e9aa9815cac918d79bce2298d5e0b5019
Hank Liang <ly604547351@163.com>
2019-08-15 09:17:17 +0800

    feat: switch Tooltip & Popover to DLS (#526)

---
+++ packages/veui/src/components/Popover.vue
@@ -0,0 +1,37 @@
+<script>
+import Tooltip from './Tooltip'
+import { getNodes } from '../utils/context'
+import ui from '../mixins/ui'
+import overlay from '../mixins/overlay'
+
+export default {
+  name: 'veui-popover',
+  mixins: [ui, overlay],
+  inheritAttrs: false,
+  computed: {
+    targetNode () {
+      return getNodes(this.$attrs.target, this.$vnode.context)[0]
+    }
+  },
+  render (h) {
+    let data = {
+      attrs: {
+        ui: this.uiParts.self || '',
+        ...this.$attrs,
+        overlayClass: this.mergeOverlayClass({
+          'veui-popover-box': true
+        }),
+        target: this.targetNode
+      },
+      on: { ...this.$listeners }
+    }
+    return h(
+      Tooltip,
+      data,
+      Object.keys(this.$slots).map(slot =>
+        h('template', { slot }, this.$slots[slot])
+      )
+    )
+  }
+}
+</script>
