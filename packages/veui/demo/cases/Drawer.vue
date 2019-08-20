0ac7fec27aa26965de8506785cfdec7cda974ab7
Justineo <justice360@gmail.com>
2019-08-20 10:00:28 +0800

    feat: switch Dialog/Drawer to DLS, add outside-closable for Dialog & backdrop-closable for Drawer

--- packages/veui/demo/cases/Drawer.vue
+++ packages/veui/demo/cases/Drawer.vue
@@ -1,27 +1,34 @@
 <template>
 <article class="demo-drawer">
   <h1><code>&lt;veui-drawer&gt;</code></h1>
-  <veui-button @click="topOpen = true">
-    打开 top Drawer
-  </veui-button>
-  <veui-button
-    ui="primary"
-    @click="rightOpen = true"
-  >
-    打开 right Drawer
-  </veui-button>
-  <veui-button @click="bottomOpen = true">
-    打开 bottom Drawer
-  </veui-button>
-  <veui-button
-    ui="primary"
-    @click="leftOpen = true"
-  >
-    打开 left Drawer
-  </veui-button>
+
+  <section>
+    <veui-checkbox v-model="modal">Modal</veui-checkbox>
+    <veui-checkbox
+      v-model="backdropClosable"
+    >Backdrop Closable</veui-checkbox>
+  </section>
+
+  <section>
+    <veui-button @click="topOpen = true">
+      Top
+    </veui-button>
+    <veui-button @click="rightOpen = true">
+      Right
+    </veui-button>
+    <veui-button @click="bottomOpen = true">
+      Bottom
+    </veui-button>
+    <veui-button @click="leftOpen = true">
+      Left
+    </veui-button>
+  </section>
+
   <veui-drawer
     :open.sync="topOpen"
     class="drawer-demo-instance"
+    :backdrop-closable="backdropClosable"
+    :modal="modal"
     placement="top"
   >
     <p>content area</p>
@@ -29,45 +36,52 @@
       slot="title"
       slot-scope="{ close }"
     >
-      <div @click="close">
+      <a @click="close">
         点我也可关闭
-      </div>
+      </a>
     </template>
-    <template
-      slot="foot"
-    >
+    <template slot="foot">
       <div>我是foot</div>
     </template>
   </veui-drawer>
   <veui-drawer
     :open.sync="rightOpen"
+    :modal="modal"
+    :backdrop-closable="backdropClosable"
     placement="right"
     title="Hey man"
   />
   <veui-drawer
     title="Hey man"
+    :modal="modal"
     :open.sync="bottomOpen"
+    :backdrop-closable="backdropClosable"
     placement="bottom"
   />
   <veui-drawer
     title="Hey man"
+    :modal="modal"
     :open.sync="leftOpen"
+    :backdrop-closable="backdropClosable"
     placement="left"
   />
 </article>
 </template>
 
 <script>
-import { Drawer, Button } from 'veui'
+import { Checkbox, Drawer, Button } from 'veui'
 
 export default {
   name: 'drawer-demo',
   components: {
     'veui-drawer': Drawer,
+    'veui-checkbox': Checkbox,
     'veui-button': Button
   },
   data () {
     return {
+      modal: true,
+      backdropClosable: true,
       topOpen: false,
       rightOpen: false,
       bottomOpen: false,
@@ -76,3 +90,18 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+section {
+  & + & {
+    margin-top: 20px;
+  }
+}
+
+.veui-button,
+.veui-checkbox {
+  & + & {
+    margin-left: 20px;
+  }
+}
+</style>
