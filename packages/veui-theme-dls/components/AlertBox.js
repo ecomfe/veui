5564755851589e3ad934953030561f700c0e2172
Justineo <justice360@gmail.com>
2019-08-22 10:49:06 +0800

    feat: switch AlertBox/ConfirmBox/PromptBox to DLS

--- packages/veui-theme-dls/components/AlertBox.js
+++ packages/veui-theme-dls/components/AlertBox.js
@@ -1,5 +1,6 @@
 import 'veui-theme-one-icons/check-circle'
 import 'veui-theme-one-icons/info-circle'
+import 'veui-theme-one-icons/exclamation-circle'
 import 'veui-theme-one-icons/times-circle'
 import config from 'veui/managers/config'
 
@@ -8,7 +9,11 @@ config.defaults(
     icons: {
       success: 'check-circle',
       info: 'info-circle',
-      error: 'times-circle'
+      error: 'times-circle',
+      warning: 'exclamation-circle'
+    },
+    parts: {
+      ok: 'primary'
     }
   },
   'alertbox'
