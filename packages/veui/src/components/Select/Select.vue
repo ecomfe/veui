634d9e42bffd0ed3f51bb9a8738d5d1cfa2fc11e
Justineo <justice360@gmail.com>
2019-08-30 14:33:38 +0800

    feat: add match-width for Overlay

--- packages/veui/src/components/Select/Select.vue
+++ packages/veui/src/components/Select/Select.vue
@@ -426,6 +426,7 @@ export default {
             options={this.realOverlayOptions}
             overlay-class={this.overlayClass}
             onLocate={this.handleRelocate}
+            match-width
           >
             <div
               id={this.dropdownId}
