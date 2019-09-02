04db18fc3df25f35d10d505af5e1dce62629ff06
Justineo <justice360@gmail.com>
2019-09-03 01:47:00 +0800

    chore: update to ava@2 and babel@7 for development, fix vulnerabilities

--- packages/babel-plugin-veui/test/index.js
+++ packages/babel-plugin-veui/test/index.js
@@ -1,18 +1,19 @@
 import test from 'ava'
-import { transformFileSync } from 'babel-core'
+import { transformFileSync } from '@babel/core'
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import plugin from '../lib'
 
-test(t => {
+test('should transform VEUI components correctly', t => {
   let { code } = transformFileSync(resolve(__dirname, './fixtures/source.js'), {
     babelrc: false,
-    plugins: [
-      plugin
-    ]
+    plugins: [plugin]
   })
 
-  let expect = readFileSync(resolve(__dirname, './fixtures/expected.js'), 'utf8')
+  let expect = readFileSync(
+    resolve(__dirname, './fixtures/expected.js'),
+    'utf8'
+  )
   expect = expect.replace(/\r\n/gm, '\n')
   code = code.replace(/\r\n/gm, '\n')
   t.is(code, expect)
