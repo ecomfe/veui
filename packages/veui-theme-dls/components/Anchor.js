584a4eeef3deaec24601f6e1d4276a2386024426
xiaodemen <fuq_pr@163.com>
2019-09-09 21:12:20 +0800

    refactor: update scrollTo & Anchor container prop

---
+++ packages/veui-theme-dls/components/Anchor.js
@@ -0,0 +1,13 @@
+import config from 'veui/managers/config'
+
+config.defaults(
+  {
+    ui: {
+      size: {
+        values: ['s', 'm'],
+        inherit: true
+      }
+    }
+  },
+  'anchor'
+)
