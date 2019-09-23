f5ae9d273d151719635e0d1e195c4ebeb694b7e0
Justineo <justice360@gmail.com>
2019-09-23 15:38:19 +0800

    fix: style and ux fix for TimePicker

--- packages/veui-theme-dls/components/TimePicker.js
+++ packages/veui-theme-dls/components/TimePicker.js
@@ -1,16 +1,21 @@
 import 'veui-theme-dls-icons/clock'
+import 'veui-theme-dls-icons/times-circle'
 import config from 'veui/managers/config'
 
 config.defaults(
   {
     icons: {
-      clock: 'clock'
+      clock: 'clock',
+      clear: 'times-circle'
     },
     ui: {
       size: {
         values: ['s', 'm'],
         inherit: true
       }
+    },
+    parts: {
+      clear: 'icon'
     }
   },
   'timepicker'
