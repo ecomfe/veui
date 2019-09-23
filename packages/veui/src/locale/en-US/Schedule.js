e30c7a122a6bb5f8ab68a726b9f7707883350117
Justineo <justice360@gmail.com>
2019-09-23 14:39:41 +0800

    fix: fix i18n for Schedule

--- packages/veui/src/locale/en-US/Schedule.js
+++ packages/veui/src/locale/en-US/Schedule.js
@@ -3,6 +3,7 @@ import i18n from '../../managers/i18n'
 i18n.register(
   'en-US',
   {
+    schedule: 'Weekly schedule picker',
     preset: 'Preset ranges',
     selectPreset: 'Select preset ranges',
     selectedRanges: 'Selected',
