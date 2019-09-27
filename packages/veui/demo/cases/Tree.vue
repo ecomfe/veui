d2c958d523626f8ac1f265f50ef383c0b3bc0bba
r7r10t32 <382141176@baidu.com>
2019-09-27 14:50:00 +0800

    feat: tree support disabled item

--- packages/veui/demo/cases/Tree.vue
+++ packages/veui/demo/cases/Tree.vue
@@ -84,110 +84,116 @@ export default {
                 },
                 {
                   label: 'Filtered',
-                  value: 'filtered'
+                  value: 'filtered',
+                  disabled: true
                 },
                 {
                   label: 'Pour-over',
-                  value: 'pour-over'
+                  value: 'pour-over',
+                  disabled: true
                 },
                 {
                   label: 'Immersion brewed',
                   value: 'immersion-brewed'
                 }
               ]
+            },
+            {
+              label: 'French press',
+              value: 'french-press'
+            },
+            {
+              label: 'Cold brew',
+              value: 'cold-brew'
             }
-            // {
-            //   label: 'French press',
-            //   value: 'french-press'
-            // },
-            // {
-            //   label: 'Cold brew',
-            //   value: 'cold-brew'
-            // }
+          ]
+        },
+        {
+          label: 'Boiled',
+          value: 'boiled',
+          disabled: true,
+          children: [
+            {
+              label: 'Percolated',
+              value: 'percolated'
+            },
+            {
+              label: 'Turkish',
+              value: 'turkish'
+            },
+            {
+              label: 'Moka',
+              value: 'moka'
+            }
+          ]
+        },
+        {
+          label: 'Espresso',
+          value: 'espresso',
+          children: [
+            {
+              label: 'Caffè Americano',
+              value: 'caffe-americano'
+            },
+            {
+              label: 'Cafe Lungo',
+              value: 'cafe-lungo',
+              disabled: true
+            },
+            {
+              label: 'Café Cubano',
+              value: 'cafe-cubano'
+            },
+            {
+              label: 'Caffè crema',
+              value: 'caffe-crema'
+            },
+            {
+              label: 'Cafe Zorro',
+              value: 'cafe-zorro',
+              disabled: true
+            },
+            {
+              label: 'Doppio',
+              value: 'doppio',
+              disabled: true
+            },
+            {
+              label: 'Espresso Romano',
+              value: 'espresso-romano'
+            },
+            {
+              label: 'Guillermo',
+              value: 'guillermo'
+            },
+            {
+              label: 'Ristretto',
+              value: 'ristretto'
+            }
+          ]
+        },
+        {
+          label: 'Milk coffee',
+          value: 'milk-coffee',
+          children: [
+            {
+              label: 'Latte',
+              value: 'latte'
+            },
+            {
+              label: 'Macchiato',
+              value: 'macchiato'
+            },
+            {
+              label: 'Cappuccino',
+              value: 'cappuccino'
+            },
+            {
+              label: 'White coffee',
+              value: 'white-coffee'
+            }
           ]
         }
-        // {
-        //   label: 'Boiled',
-        //   value: 'boiled',
-        //   children: [
-        //     {
-        //       label: 'Percolated',
-        //       value: 'percolated'
-        //     },
-        //     {
-        //       label: 'Turkish',
-        //       value: 'turkish'
-        //     },
-        //     {
-        //       label: 'Moka',
-        //       value: 'moka'
-        //     }
-        //   ]
-        // },
-        // {
-        //   label: 'Espresso',
-        //   value: 'espresso',
-        //   children: [
-        //     {
-        //       label: 'Caffè Americano',
-        //       value: 'caffe-americano'
-        //     },
-        //     {
-        //       label: 'Cafe Lungo',
-        //       value: 'cafe-lungo'
-        //     },
-        //     {
-        //       label: 'Café Cubano',
-        //       value: 'cafe-cubano'
-        //     },
-        //     {
-        //       label: 'Caffè crema',
-        //       value: 'caffe-crema'
-        //     },
-        //     {
-        //       label: 'Cafe Zorro',
-        //       value: 'cafe-zorro'
-        //     },
-        //     {
-        //       label: 'Doppio',
-        //       value: 'doppio'
-        //     },
-        //     {
-        //       label: 'Espresso Romano',
-        //       value: 'espresso-romano'
-        //     },
-        //     {
-        //       label: 'Guillermo',
-        //       value: 'guillermo'
-        //     },
-        //     {
-        //       label: 'Ristretto',
-        //       value: 'ristretto'
-        //     }
-        //   ]
-        // },
-        // {
-        //   label: 'Milk coffee',
-        //   value: 'milk-coffee',
-        //   children: [
-        //     {
-        //       label: 'Latte',
-        //       value: 'latte'
-        //     },
-        //     {
-        //       label: 'Macchiato',
-        //       value: 'macchiato'
-        //     },
-        //     {
-        //       label: 'Cappuccino',
-        //       value: 'cappuccino'
-        //     },
-        //     {
-        //       label: 'White coffee',
-        //       value: 'white-coffee'
-        //     }
-        //   ]
-        // }
       ]
     }
   }
