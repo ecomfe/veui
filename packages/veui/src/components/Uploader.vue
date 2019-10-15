7fd71b396baef88e7a81a567274996aac7d55abe
r7r10t32 <382141176@baidu.com>
2019-10-15 17:21:18 +0800

    fix: fix input hidden and remove stringifyQuery

--- packages/veui/src/components/Uploader.vue
+++ packages/veui/src/components/Uploader.vue
@@ -407,13 +407,13 @@
       :key="key"
       :name="key"
       :value="val"
-      hidden
+      type="hidden"
     >
     <input
       v-if="iframeMode === 'callback'"
       name="callback"
       :value="`parent.${callbackNamespace}['${callbackFuncName}']`"
-      hidden
+      type="hidden"
     >
   </form>
 </div>
