c0eabdf485c33b0c684aaa6d6eb85d75cd2698f0
Justineo <justice360@gmail.com>
2019-11-06 20:27:13 +0800

    feat: update vue-awesome and simplify icon implementation

--- packages/veui/src/components/Icon.vue
+++ packages/veui/src/components/Icon.vue
@@ -1,22 +1,14 @@
 <script>
 import FaIcon from 'vue-awesome/components/Icon'
-import prefix from '../mixins/prefix'
+import { prefixify } from '../mixins/prefix'
 
 export default {
   ...FaIcon,
   name: 'veui-icon',
-  mixins: [prefix],
-  computed: {
-    ...FaIcon.computed,
-    klass () {
-      return {
-        'fa-icon': true,
-        'fa-spin': this.spin,
-        'fa-flip-horizontal': this.flip === 'horizontal',
-        'fa-flip-vertical': this.flip === 'vertical',
-        'fa-inverse': this.inverse,
-        'fa-pulse': this.pulse,
-        [this.$c('icon')]: true
+  data () {
+    return {
+      classes: {
+        [prefixify('icon')]: true
       }
     }
   }
--- packages/veui/src/mixins/prefix.js
+++ packages/veui/src/mixins/prefix.js
@@ -1,4 +1,4 @@
-function prefixify (name) {
+export function prefixify (name) {
   return `${process.env.VEUI_PREFIX ||
     process.env.VUE_APP_VEUI_PREFIX ||
     'veui'}-${name}`
