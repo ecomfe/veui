219f7f4a3171e27888af27887c3ef27e6b074dc1
Justineo <justice360@gmail.com>
2019-12-27 18:51:41 +0800

    test: fix move link and unlink into test cases

--- packages/babel-plugin-veui/test/index.js
+++ packages/babel-plugin-veui/test/index.js
@@ -1,25 +1,43 @@
 import test from 'ava'
 import { transformFileSync } from '@babel/core'
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
+import { readFileSync, symlink, unlink } from 'fs'
+import { resolve as r } from 'path'
 import prettier from 'prettier'
 import plugin from '../lib'
 
 test('should transform VEUI components correctly', t => {
-  let { code } = transformFileSync(resolve(__dirname, './fixtures/source.js'), {
-    babelrc: false,
-    plugins: [plugin, [plugin, { name: 'veui-next' }, 'veui-next']]
-  })
+  return prepare()
+    .then(() => {
+      let { code } = transformFileSync(r(__dirname, './fixtures/source.js'), {
+        babelrc: false,
+        plugins: [plugin, [plugin, { name: 'veui-next' }, 'veui-next']]
+      })
 
-  let expect = readFileSync(
-    resolve(__dirname, './fixtures/expected.js'),
-    'utf8'
-  )
-  expect = expect.replace(/\r\n/gm, '\n')
-  code = code.replace(/\r\n/gm, '\n')
-  t.is(format(code), format(expect))
+      let expect = readFileSync(r(__dirname, './fixtures/expected.js'), 'utf8')
+      expect = expect.replace(/\r\n/gm, '\n')
+      code = code.replace(/\r\n/gm, '\n')
+
+      t.is(format(code), format(expect))
+    })
+    .then(tearDown)
 })
 
 function format (source) {
   return prettier.format(source, { parser: 'babel' })
+}
+
+function prepare () {
+  return new Promise(resolve => {
+    symlink(
+      r(__dirname, '../node_modules/veui'),
+      r(__dirname, '../node_modules/veui-next'),
+      resolve
+    )
+  })
+}
+
+function tearDown () {
+  return new Promise(resolve => {
+    unlink(r(__dirname, '../node_modules/veui-next'), resolve)
+  })
 }
--- packages/babel-plugin-veui/test/link.js
+++
@@ -1,8 +0,0 @@
-import { symlink } from 'fs'
-import { resolve } from 'path'
-
-symlink(
-  resolve(__dirname, '../node_modules/veui'),
-  resolve(__dirname, '../node_modules/veui-next'),
-  () => {}
-)
