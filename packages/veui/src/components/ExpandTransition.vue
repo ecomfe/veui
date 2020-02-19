30c5e3c7893d20514865aee4f909bff17ad21dc0
xiaodemen <fuq_pr@163.com>
2020-02-19 21:05:49 +0800

    fix: add expand transition

---
+++ packages/veui/src/components/ExpandTransition.vue
@@ -0,0 +1,57 @@
+<template>
+<transition
+  :name="name"
+  @before-enter="beforeEnter"
+  @enter="enter"
+  @after-enter="afterEnter"
+  @before-leave="beforeLeave"
+  @leave="leave"
+  @after-leave="afterLeave"
+>
+  <slot/>
+</transition>
+</template>
+
+<script>
+export default {
+  name: 'veui-expand-transition',
+  abstract: true,
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    // 一旦覆盖 hook 了，是不是就没必要用这个组件了？
+    beforeEnter (el) {
+      el.style.height = '0'
+      this.$emit('before-enter', el)
+    },
+    enter (el) {
+      if (el.scrollHeight) {
+        el.style.height = `${el.scrollHeight}px`
+      }
+      this.$emit('enter', el)
+    },
+    afterEnter (el) {
+      el.style.height = ''
+      this.$emit('after-enter', el)
+    },
+    beforeLeave (el) {
+      el.style.height = `${el.scrollHeight}px`
+      this.$emit('before-leave', el)
+    },
+    leave (el) {
+      if (el.scrollHeight) {
+        el.style.height = '0'
+      }
+      this.$emit('leave', el)
+    },
+    afterLeave (el) {
+      el.style.height = ''
+      this.$emit('after-leave', el)
+    }
+  }
+}
+</script>
