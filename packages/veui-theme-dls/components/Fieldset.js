4f2c40488328ed7decd79337fc62c4fccc470f5f
feibinyang <feibinyang@gmail.com>
2020-05-18 17:13:38 +0800

    feat: update Form/Fieldset/Field styles to d20 (#666)

--- packages/veui-theme-dls/components/Fieldset.js
+++ packages/veui-theme-dls/components/Fieldset.js
@@ -1,12 +1,3 @@
 import config from 'veui/managers/config'
 
-config.defaults(
-  {
-    ui: {
-      style: {
-        values: ['alt']
-      }
-    }
-  },
-  'fieldset'
-)
+config.defaults({}, 'fieldset')
