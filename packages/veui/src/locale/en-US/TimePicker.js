601fbbf9cc383cd8e23be2e75e34f69aaf34218b
Justineo <justice360@gmail.com>
2020-07-08 00:35:54 +0800

    feat: add i18n for TimePicker, add placeholder config and stops autocomplete

---
+++ packages/veui/src/locale/en-US/TimePicker.js
@@ -0,0 +1,12 @@
+import i18n from '../../managers/i18n'
+
+i18n.register(
+  'en-US',
+  {
+    placeholder: 'Pick a time',
+    clear: 'Clear'
+  },
+  {
+    ns: 'timepicker'
+  }
+)
---
+++ packages/veui/src/locale/zh-Hans/TimePicker.js
@@ -0,0 +1,12 @@
+import i18n from '../../managers/i18n'
+
+i18n.register(
+  'zh-Hans',
+  {
+    placeholder: '请选择时间',
+    clear: '清除'
+  },
+  {
+    ns: 'timepicker'
+  }
+)
