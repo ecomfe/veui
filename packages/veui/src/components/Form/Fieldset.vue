7a7bb4ab7d2097fe26144403a19e338b2e25f107
Justineo <justice360@gmail.com>
2020-08-09 02:23:22 +0800

    refactor: remove unnecessary code

--- packages/veui/src/components/Form/Fieldset.vue
+++ packages/veui/src/components/Form/Fieldset.vue
@@ -39,10 +39,7 @@ export default {
     disabled: Boolean,
     readonly: Boolean,
     // 因为会出现一行里边有必填和非必填共存，交给使用者决定显不显示星号
-    required: {
-      type: Boolean,
-      default: false
-    }
+    required: Boolean
   },
   computed: {
     attrs () {
