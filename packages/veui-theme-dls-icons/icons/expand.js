7198cff63016fbba400200f474d9c290f8871a54
Justineo <justice360@gmail.com>
2020-08-28 16:01:10 +0800

    feat: update icons

---
+++ packages/veui-theme-dls-icons/icons/expand.js
@@ -0,0 +1,4 @@
+import Icon from 'veui/components/Icon'
+import { IconExpand } from 'dls-icons-vue'
+
+Icon.register('expand', IconExpand)
---
+++ packages/veui-theme-dls-icons/icons/one-to-one.js
@@ -0,0 +1,4 @@
+import Icon from 'veui/components/Icon'
+import { IconOneToOne } from 'dls-icons-vue'
+
+Icon.register('one-to-one', IconOneToOne)
