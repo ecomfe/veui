3a07d232c1b9fb6caacadfa9f17c74622af7625a
Justineo <justice360@gmail.com>
2020-09-09 01:36:53 +0800

    fix(i18n): fix transfer select/deselect all chinese label

--- packages/veui/src/locale/zh-Hans/Transfer.js
+++ packages/veui/src/locale/zh-Hans/Transfer.js
@@ -5,8 +5,8 @@ i18n.register(
   {
     available: '待选项',
     selected: '已选项',
-    selectAll: '选择全部',
-    deselectAll: '移除全部',
+    selectAll: '全选',
+    deselectAll: '清空',
     expand: '展开项目',
     collapse: '收起项目',
     noData: '没有待选项',
