eeb3c2c072d34fe78a3ecd9407a0c6f03ccba6ed
poppyLuo <wen_wenjin_ok@126.com>
2020-11-06 14:45:50 +0800

    * fix: fix unit test for uploader

--- packages/veui/test/unit/specs/components/Uploader.spec.js
+++ packages/veui/test/unit/specs/components/Uploader.spec.js
@@ -25,7 +25,7 @@ describe('components/Uploader', () => {
     })
 
     expect(wrapper.vm.$data.fileList).to.eql([
-      { name: 'test.jpg', src: '/test.jpg', status: 'success' }
+      { name: 'test.jpg', src: '/test.jpg', status: 'success', type: 'image' }
     ])
     wrapper.destroy()
   })
@@ -565,7 +565,7 @@ describe('components/Uploader', () => {
       .trigger('click')
 
     expect(wrapper.emitted().test[0][0]).to.eql(
-      { name: 'test1.jpg', src: '/test1.jpg', status: 'success' },
+      { name: 'test1.jpg', src: '/test1.jpg', status: 'success', type: 'image' },
       0
     )
     wrapper.destroy()
