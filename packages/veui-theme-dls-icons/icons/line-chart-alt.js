50371ccd0e6c8897da6fec5f963a96e1501ca0a8
Justineo <justice360@gmail.com>
2021-01-14 17:42:30 +0800

    chore: update deps and version

---
+++ packages/veui-theme-dls-icons/icons/line-chart-alt.js
@@ -0,0 +1,4 @@
+import Icon from 'veui/components/Icon'
+import { IconLineChartAlt } from 'dls-icons-vue'
+
+Icon.register('line-chart-alt', IconLineChartAlt)
---
+++ packages/veui-theme-dls-icons/icons/video-history.js
@@ -0,0 +1,4 @@
+import Icon from 'veui/components/Icon'
+import { IconVideoHistory } from 'dls-icons-vue'
+
+Icon.register('video-history', IconVideoHistory)
