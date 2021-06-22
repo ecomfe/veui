ba98e44d73215f31ddd3ca1f474085b00c3c2fc2
xiaodemen <fuq_pr@163.com>
2021-06-22 17:25:04 +0800

    chore: add package exports for veui

---
+++ packages/veui/src/mixins/exports.js
@@ -0,0 +1,2 @@
+export { default as useControllable } from './controllable'
+export { default as useSearchable } from './searchable'
---
+++ packages/veui/src/plugins/index.js
@@ -0,0 +1,4 @@
+export { default as alert } from './alert'
+export { default as confirm } from './confirm'
+export { default as prompt } from './prompt'
+export { default as toast } from './toast'
