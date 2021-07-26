2f3f2a445dd1923380f0f88f37daefdf730027a2
Justineo <justice360@gmail.com>
2021-07-26 19:52:34 +0800

    fix: toast plugin calls should accept `options` param (#861)

--- packages/veui/test/unit/specs/plugins/toast.spec.js
+++ packages/veui/test/unit/specs/plugins/toast.spec.js
@@ -1,5 +1,6 @@
-import { createLocalVue } from '@vue/test-utils'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
 import toastPlugin from '@/plugins/toast'
+import { wait } from '../../../utils'
 
 const localVue = createLocalVue()
 
@@ -12,5 +13,34 @@ describe('plugins/toast', () => {
     expect(localVue.prototype.$toast.info).to.be.a('function')
     expect(localVue.prototype.$toast.warn).to.be.a('function')
     expect(localVue.prototype.$toast.error).to.be.a('function')
+  })
+
+  it('should accept an optional `options` param', async () => {
+    shallowMount(
+      {
+        template: '<div></div>',
+        mounted () {
+          this.$toast.success('🟩', { duration: 200 })
+          this.$toast.info('🟦', { duration: 200 })
+          this.$toast.warn('🟨', { duration: 200 })
+          this.$toast.error('🟥', { duration: 200 })
+        }
+      },
+      { localVue }
+    )
+
+    await wait(0)
+
+    expect(document.querySelectorAll('.veui-toast-success').length).to.equal(1)
+    expect(document.querySelectorAll('.veui-toast-info').length).to.equal(1)
+    expect(document.querySelectorAll('.veui-toast-warning').length).to.equal(1)
+    expect(document.querySelectorAll('.veui-toast-error').length).to.equal(1)
+
+    await wait(500)
+
+    expect(document.querySelectorAll('.veui-toast-success').length).to.equal(0)
+    expect(document.querySelectorAll('.veui-toast-info').length).to.equal(0)
+    expect(document.querySelectorAll('.veui-toast-warning').length).to.equal(0)
+    expect(document.querySelectorAll('.veui-toast-error').length).to.equal(0)
   })
 })
