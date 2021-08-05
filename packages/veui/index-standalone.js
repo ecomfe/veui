4fd161a69ef8699b5051d127967c3f560fcb33e5
Justineo <justice360@gmail.com>
2021-08-05 11:21:34 +0800

    fix: add missing standalone entry file

---
+++ packages/veui/index-standalone.js
@@ -0,0 +1,22 @@
+import './locale/zh-Hans'
+import './locale/en-US'
+import * as components from './components/index'
+import * as plugins from './plugins/index'
+
+export * from './index'
+
+export function install (Vue, { prefix } = {}) {
+  Object.keys(components).forEach(key => {
+    const component = components[key]
+    let name = component.name
+    if (typeof prefix === 'string') {
+      name = component.name.replace(/^veui-/, prefix ? `${prefix}-` : '')
+    }
+    Vue.component(name, component)
+  })
+
+  Object.keys(plugins).forEach(key => {
+    const plugin = plugins[key]
+    Vue.use(plugin)
+  })
+}
