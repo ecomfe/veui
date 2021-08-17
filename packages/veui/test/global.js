5422e86c5585235ae75958b7629c6412e2f058f5
Justineo <justice360@gmail.com>
2021-08-17 10:57:16 +0800

    test: ignore resizeobserver loop exceeded error for tests

--- packages/veui/test/global.js
+++ packages/veui/test/global.js
@@ -1,2 +1,24 @@
 import 'focus-visible'
 import 'veui-theme-dls/common.less'
+
+// Safely ignore ResizeObserver loop limit exceeded
+// https://github.com/WICG/resize-observer/issues/38
+let onerror
+
+before(() => {
+  onerror = window.onerror
+
+  window.onerror = (...args) => {
+    const [e] = args
+    if (e === 'ResizeObserver loop limit exceeded') {
+      console.warn(`[test] ${e}`)
+      return false
+    } else if (onerror) {
+      return onerror(...args)
+    }
+  }
+})
+
+after(() => {
+  window.onerror = onerror
+})
