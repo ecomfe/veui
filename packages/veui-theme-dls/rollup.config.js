46ed588fab26756cace492b61480856450fca348
xiaodemen <fuq_pr@163.com>
2021-09-14 18:06:19 +0800

    fix: fix theme dls esm bundle

--- packages/veui-theme-dls/rollup.config.js
+++ packages/veui-theme-dls/rollup.config.js
@@ -4,21 +4,24 @@ import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
 import LessTildeFileManager from './build/LessTildeFileManager'
 
-const commonPlugins = [
-  {
+function transformVeuiConfig (veuiId) {
+  return {
     name: 'transform-veui-config',
     resolveId (source) {
-      if (source === 'veui/managers/config') {
+      if (source === 'veui/managers/ui') {
         return '\0virtual-config'
       }
       return null
     },
     load (id) {
       return id === '\0virtual-config'
-        ? 'import { config } from "veui";\nexport default config;'
+        ? `import { ui } from "${veuiId}";\nexport default ui;`
         : null
     }
-  },
+  }
+}
+
+const commonPlugins = [
   replace({
     preventAssignment: true,
     'process.env.NODE_ENV': '"development"'
@@ -44,21 +47,28 @@ const commonPlugins = [
   })
 ]
 
-export default {
-  input: 'index.js',
-  output: [
-    {
+const externals = ['veui', 'veui/dist/veui.esm']
+
+export default [
+  {
+    input: 'index.js',
+    output: {
       file: 'dist/dls.js',
       format: 'umd',
       globals: {
         veui: 'veui'
       }
     },
-    {
+    plugins: [transformVeuiConfig('veui'), ...commonPlugins],
+    external: id => externals.indexOf(id) >= 0
+  },
+  {
+    input: 'index.js',
+    output: {
       file: 'dist/dls.esm.js',
       format: 'esm'
-    }
-  ],
-  plugins: commonPlugins,
-  external: id => id === 'veui'
-}
+    },
+    plugins: [transformVeuiConfig('veui/dist/veui.esm'), ...commonPlugins],
+    external: id => externals.indexOf(id) >= 0
+  }
+]
--- packages/veui/src/index.js
+++ packages/veui/src/index.js
@@ -2,6 +2,7 @@
 export * from './components/index'
 
 export { default as config } from './managers/config'
+export { default as ui } from './managers/ui'
 export { default as i18n } from './managers/i18n'
 export { default as validation } from './managers/rule'
 export { default as alert } from './managers/alert'
