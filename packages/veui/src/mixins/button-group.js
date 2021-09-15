760f5cc4a91b434ec5cb9f9f1f3a0ead5926d503
xiaodemen <fuq_pr@163.com>
2021-09-15 18:43:37 +0800

    refactor: useDisabledDesc -> useDesc

--- packages/veui/src/mixins/button-group.js
+++ packages/veui/src/mixins/button-group.js
@@ -7,11 +7,7 @@ export default {
   },
   methods: {
     handleEnterForDesc (item) {
-      let disabled = item.disabled || this.realDisabled || this.realReadonly
-      if (
-        disabled &&
-        (item.desc || this.$scopedSlots.desc || this.$slots.desc)
-      ) {
+      if (item.desc || this.$scopedSlots.desc || this.$slots.desc) {
         // 不能用字符串 ref，因为字符串不变，但是 Button 在切换 disabled 时 dom 会变化
         // 在切换 target 时，popover 是无法通过 hover 来自动 open 的，所以手动要 open
         this.openForDesc = true
