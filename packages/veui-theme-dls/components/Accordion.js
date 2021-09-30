dc9bb56542e05fff22ab6c65fcac38c5df58c6ae
xiaodemen <fuq_pr@163.com>
2021-09-30 11:36:36 +0800

    feat: add ui variants for Collapse and separate Accordion

--- packages/veui-theme-dls/components/Accordion.js
+++ packages/veui-theme-dls/components/Accordion.js
@@ -5,7 +5,23 @@ ui.defaults(
     ui: {
       size: {
         values: ['s', 'm'],
+        default: 'm',
         inherit: true
+      },
+      style: {
+        values: ['normal', 'simple', 'basic', 'strong'],
+        inherit: true
+      },
+      border: {
+        values: ['borderless', 'bordered'],
+        inherit: true
+      },
+      dull: {
+        boolean: true,
+        inherit: true
+      },
+      separate: {
+        boolean: true
       }
     }
   },
