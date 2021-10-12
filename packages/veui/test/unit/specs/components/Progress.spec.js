3d1b357d27781c2d61a98356a07f09b4dcbc78ec
xiaodemen <fuq_pr@163.com>
2021-10-12 15:00:40 +0800

    fix: fix progress status

---
+++ packages/veui/test/unit/specs/components/Progress.spec.js
@@ -0,0 +1,106 @@
+import Progress from '@/components/Progress'
+import { wait, mount } from '../../../utils'
+
+describe('components/Progress', function () {
+  it('should handle value/min/max prop correctly.', async () => {
+    let wrapper = mount({
+      components: {
+        'veui-progress': Progress
+      },
+      data () {
+        return {
+          value: 50
+        }
+      },
+      template: `
+        <veui-progress :value="value" :min="0" :max="100"/>`
+    })
+
+    const { vm } = wrapper
+    await wait(0)
+
+    let progress = wrapper.find('.veui-progress')
+    expect(progress.classes()).to.not.contains('veui-progress-status-complete')
+
+    vm.value = 100
+    await wait(0)
+    progress = wrapper.find('.veui-progress')
+    expect(progress.classes()).to.contains('veui-progress-status-complete')
+
+    wrapper.destroy()
+  })
+
+  it('should handle desc/decimalPlace prop correctly.', async () => {
+    let wrapper = mount({
+      components: {
+        'veui-progress': Progress
+      },
+      data () {
+        return {
+          value: 50,
+          desc: false,
+          decimalPlace: null
+        }
+      },
+      template: `
+        <veui-progress :desc="desc" :decimal-place="decimalPlace" :value="value" :min="0" :max="100"/>`
+    })
+
+    const { vm } = wrapper
+
+    await wait(0)
+    testDesc(false)
+
+    vm.desc = true
+    await wait(0)
+    testDesc(true, false)
+
+    vm.decimalPlace = 1
+    await wait(0)
+    testDesc(true, true)
+
+    function testDesc (exists, decimal) {
+      let desc = wrapper.find('.veui-progress-desc')
+      expect(desc.exists()).to.equal(exists)
+      if (decimal != null) {
+        expect(!!desc.text().match(/\./)).to.equal(decimal)
+      }
+    }
+    wrapper.destroy()
+  })
+
+  it('should handle status/autosucceed prop correctly.', async () => {
+    let wrapper = mount({
+      components: {
+        'veui-progress': Progress
+      },
+      data () {
+        return {
+          value: 100,
+          status: null,
+          autosucceed: false
+        }
+      },
+      template: `
+        <veui-progress :status.sync="status" :autosucceed="autosucceed" :value="value" :min="0" :max="100"/>`
+    })
+
+    const { vm } = wrapper
+
+    await wait(0)
+    let progress = wrapper.find('.veui-progress')
+    expect(progress.classes()).to.contains('veui-progress-status-complete')
+    expect(progress.classes()).to.not.contains('veui-progress-status-success')
+    expect(vm.status).to.equal(null)
+
+    vm.autosucceed = true
+    await wait(0)
+    expect(vm.status).to.equal('success')
+
+    vm.value = 99
+    await wait(0)
+    expect(vm.status).to.equal(null)
+
+    wrapper.destroy()
+  })
+})
