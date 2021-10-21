d6ad6bb96e98500fde21596d26052e915a113d77
xiaodemen <fuq_pr@163.com>
2021-10-21 14:20:21 +0800

    chore: upgrade less-plugin-dls

--- packages/veui-theme-dls/components/Tooltip.js
+++ packages/veui-theme-dls/components/Tooltip.js
@@ -3,6 +3,11 @@ import ui from 'veui/managers/ui'
 ui.defaults(
   {
     ui: {
+      size: {
+        values: ['s', 'm'],
+        default: 'm',
+        inherit: true
+      },
       reverse: {
         boolean: true
       }
