bc4f4dd83dbb5041819ecd9ab9420a2f8ba17558
Justineo <justice360@gmail.com>
2021-11-12 20:02:20 +0800

    feat: add overflow tooltip for tooltip directive and table column

--- packages/veui/src/directives/tooltip.js
+++ packages/veui/src/directives/tooltip.js
@@ -1,13 +1,15 @@
 import { normalize } from 'vue-directive-normalizer'
 import tooltip from '../managers/tooltip'
 import { flatMap } from 'lodash'
+import { isOverflow } from '../utils/dom'
 
 const OPTIONS_SCHEMA = {
   value: 'content',
   modifiers: {
     position: flatMap(['top', 'right', 'bottom', 'left', 'auto'], side => {
       return [side, `${side}-start`, `${side}-end`]
-    })
+    }),
+    overflow: false
   },
   defaults: () => ({
     disabled: false
@@ -28,7 +30,10 @@ function refresh (el, binding) {
     if (options.disabled) {
       return
     }
-    tooltip.enter(this, options)
+
+    if (!options.overflow || isOverflow(el)) {
+      tooltip.enter(this, options)
+    }
   }
 
   function handleMouseLeave () {
@@ -36,7 +41,9 @@ function refresh (el, binding) {
     if (options.disabled) {
       return
     }
-    tooltip.leave()
+    if (!options.overflow || isOverflow(el)) {
+      tooltip.leave()
+    }
   }
 
   context = el.__tooltip_context__ = {
@@ -52,7 +59,7 @@ function refresh (el, binding) {
 function clear (el) {
   const { handleMouseEnter, handleMouseLeave } = el.__tooltip_context__
 
-  tooltip.leave(el)
+  tooltip.destroy()
   el.removeEventListener('mouseenter', handleMouseEnter)
   el.removeEventListener('mouseleave', handleMouseLeave)
 
