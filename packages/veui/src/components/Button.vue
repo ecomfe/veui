1213effb58ebfe5200ddaab9ac5e4f1ef1ce310d
Justineo <justice360@gmail.com>
2021-12-30 19:21:26 +0800

    refactor: use div > button to implement veui-button to retain transition when toggling disabled

--- packages/veui/src/components/Button.vue
+++ packages/veui/src/components/Button.vue
@@ -6,37 +6,56 @@ import ui from '../mixins/ui'
 import focusable from '../mixins/focusable'
 import '../common/global'
 
+const BUTTON_ATTRS = new Set([
+  'form',
+  'formaction',
+  'formenctype',
+  'formmethod',
+  'formnovalidate',
+  'formtarget',
+  'name',
+  'value'
+])
+
 export default {
   name: 'veui-button',
   components: {
     'veui-icon': Icon
   },
   mixins: [prefix, ui, focusable],
+  inheritAttrs: false,
   props: {
     disabled: Boolean,
-    name: String,
     type: {
       type: String,
       default: 'button'
     },
-    value: String,
     loading: Boolean
+  },
+  data () {
+    return {
+      active: false
+    }
   },
   computed: {
     activatable () {
       return !this.disabled && !this.loading
     },
     attrs () {
-      let { loading, disabled, type, ...props } = this.$props
+      const button = {}
+      const root = {}
+
+      Object.keys(this.$attrs).forEach(key => {
+        if (BUTTON_ATTRS.has(key)) {
+          button[key] = this.$attrs[key]
+        } else {
+          root[key] = this.$attrs[key]
+        }
+      })
 
       return {
-        tabindex: this.activatable ? null : '0',
-        role: this.activatable ? null : 'button',
-        ...props,
-        // 渲染成 div 的时候，type="button" 会导致在 Safari 上样式出现问题
-        type: this.activatable ? type : null,
-        'aria-disabled': this.disabled ? 'true' : null,
-        ui: this.realUi
+        button,
+        root
       }
     },
     listeners () {
@@ -51,40 +70,91 @@ export default {
   methods: {
     focus () {
       this.$el.focus()
+    },
+    handleKeydown (e) {
+      if (!this.activatable) {
+        return
+      }
+
+      if (e.key === ' ') {
+        this.active = true
+      }
+    },
+    handleKeyup (e) {
+      if (!this.activatable) {
+        return
+      }
+
+      if (this.active && e.key === ' ') {
+        this.$refs.button.click()
+        this.active = false
+      }
+    },
+    handleKeypress (e) {
+      if (this.activatable && e.key === 'Enter') {
+        this.$refs.button.click()
+      } else if (e.key === ' ') {
+        e.preventDefault() // prevent page scroll
+      }
+    },
+    handleBlur () {
+      this.active = false
     }
   },
   render () {
-    let Tag = this.activatable ? 'button' : 'div'
     return (
-      <Tag
+      <div
+        role="button"
+        tabindex="0"
         class={{
           [this.$c('button')]: true,
           [this.$c('button-loading')]: this.loading,
-          [this.$c('disabled')]: this.disabled
+          [this.$c('disabled')]: this.disabled,
+          [this.$c('active')]: this.active
         }}
+        aria-disabled={this.disabled ? 'true' : null}
+        ui={this.realUi}
+        onKeydown={this.handleKeydown}
+        onKeyup={this.handleKeyup}
+        onKeypress={this.handleKeypress}
+        onBlur={this.handleBlur}
         {...{
-          attrs: this.attrs,
+          attrs: this.attrs.root,
           on: this.listeners
         }}
       >
-        {this.loading ? (
-          this.icons.loading ? (
-            <Icon
-              class={this.$c('button-loading-icon')}
-              name={this.icons.loading}
-              spin
-            />
-          ) : (
-            <Loading class={this.$c('button-loading-icon')} loading />
-          )
-        ) : null}
-        {(this.$scopedSlots.default
-          ? this.$scopedSlots.default()
-          : this.$slots.default || []
-        ).map(vnode =>
-          !vnode.tag && (vnode.text || '').trim() ? <span>{vnode}</span> : vnode
-        )}
-      </Tag>
+        <button
+          ref="button"
+          class={this.$c('button-native')}
+          tabindex="-1"
+          role="presentation"
+          disabled={!this.activatable}
+          type={this.type}
+          {...{ attrs: this.attrs.button }}
+        >
+          {this.loading ? (
+            this.icons.loading ? (
+              <Icon
+                class={this.$c('button-loading-icon')}
+                name={this.icons.loading}
+                spin
+              />
+            ) : (
+              <Loading class={this.$c('button-loading-icon')} loading />
+            )
+          ) : null}
+          {(this.$scopedSlots.default
+            ? this.$scopedSlots.default()
+            : this.$slots.default || []
+          ).map(vnode =>
+            !vnode.tag && (vnode.text || '').trim() ? (
+              <span>{vnode}</span>
+            ) : (
+              vnode
+            )
+          )}
+        </button>
+      </div>
     )
   }
 }
