e7ead496c5b8b37d32f484ee1e79445cb2dcd8b9
Justineo <justice360@gmail.com>
2022-09-27 17:55:35 +0800

    feat: add dot variant and stateless prop for steps

--- packages/veui/src/components/Steps.vue
+++ packages/veui/src/components/Steps.vue
@@ -3,22 +3,23 @@
   <template v-for="(step, index) in steps">
     <div
       v-if="index !== 0"
-      :key="'connector-' + index"
+      :key="`connector-${index}`"
       :class="[
         $c('steps-step-connector'),
         $c(`steps-step-connector-${stepStatus[index]}`)
       ]"
     />
     <veui-link
-      :key="'step-' + index"
+      :key="`step-${index}`"
       :class="[
         $c('steps-step'),
         $c(`steps-step-${stepStatus[index]}`),
         index < steps.length - 1
           ? $c(`steps-step-next-${stepStatus[index + 1]}`)
-          : ''
+          : '',
+        { [$c('steps-step-activatable')]: !stateless && !!step.to }
       ]"
-      :to="step.to"
+      :to="stateless ? null : step.to"
       fallback="div"
       role="listitem"
       :aria-current="index === current ? 'step' : null"
@@ -29,11 +30,13 @@
           <div :class="$c('steps-step-index')">
             <slot name="index" v-bind="step" :index="index">
               <veui-icon
-                v-if="index < current && step.status !== 'error'"
+                v-if="
+                  !stateless && index < current && step.status !== 'error'
+                "
                 :name="icons.success"
               />
               <veui-icon
-                v-else-if="step.status === 'error'"
+                v-else-if="!stateless && step.status === 'error'"
                 :name="icons.error"
               />
               <template v-else>{{ index + 1 }}</template>
@@ -48,16 +51,16 @@
           />
         </div>
         <div v-if="step.label" :class="$c('steps-step-content')">
-          <h3 :class="$c('steps-step-label')">
+          <div :class="$c('steps-step-label')">
             <slot name="label" v-bind="step" :index="index">
               {{ step.label }}
             </slot>
-          </h3>
-          <p v-if="step.desc" :class="$c('steps-step-desc')">
+          </div>
+          <div v-if="step.desc" :class="$c('steps-step-desc')">
             <slot name="desc" v-bind="step" :index="index">
               {{ step.desc }}
             </slot>
-          </p>
+          </div>
         </div>
       </slot>
     </veui-link>
@@ -90,10 +93,15 @@ export default {
     current: {
       type: Number,
       default: 0
-    }
+    },
+    stateless: Boolean
   },
   computed: {
     stepStatus () {
+      if (this.stateless) {
+        return Array(this.steps.length).fill('stateless')
+      }
+
       return this.steps.reduce((acc, step, i) => {
         let status =
           step.status === 'error'
