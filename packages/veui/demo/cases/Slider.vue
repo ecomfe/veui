2a07a6fb0ea5216ae3beaf6f554af8f3d916fec2
libeibei02 <libeibei02@macdeMacBook-Pro.local>
2023-06-02 15:42:31 +0800

    feat: add vertical support for slider

--- packages/veui/demo/cases/Slider.vue
+++ packages/veui/demo/cases/Slider.vue
@@ -109,6 +109,13 @@
       <span>{{ formatDuration(videoDuration) }}</span>
     </div>
   </section>
+
+  <section>
+    <h3>支持竖向，自定义高度</h3>
+    <veui-slider v-model="valueVertical" vertical style="height: 200px"/>
+    <h3>竖向区间，默认高度</h3>
+    <veui-slider v-model="value4" :min="0" :max="100" vertical/>
+  </section>
 </article>
 </template>
 
@@ -131,6 +138,7 @@ export default {
       value2: 333,
       value3: makeArray(5).map((_, i) => `hsl(${(i + 1) * 60}, 100%, 50%)`),
       value4: [22, 66],
+      valueVertical: 0.5,
 
       videoPlayProgress: 0.11,
       videoBufferProgress: 0.57,
