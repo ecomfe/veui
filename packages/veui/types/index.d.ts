a1fd456c55d37e0ed33b3a3c75e2ffc053423533
Justineo <justice360@gmail.com>
2023-07-07 18:28:12 +0800

    feat: enhance ui manager, implements #565

--- packages/veui/types/index.d.ts
+++ packages/veui/types/index.d.ts
@@ -1,4 +1,5 @@
 import * as Components from './components'
+export * from './managers'
 
 export declare const Accordion: Components.Accordion
 export declare const Alert: Components.Alert
@@ -13,32 +14,36 @@ export declare const ButtonGroup: Components.ButtonGroup
 export declare const Calendar: Components.Calendar
 export declare const Carousel: Components.Carousel
 export declare const Cascader: Components.Cascader
-export declare const CheckButtonGroup: Components.CheckButtonGroup
 export declare const Checkbox: Components.Checkbox
 export declare const CheckboxGroup: Components.CheckboxGroup
+export declare const CheckButtonGroup: Components.CheckButtonGroup
 export declare const Collapse: Components.Collapse
 export declare const Column: Components.Column
 export declare const ConfigProvider: Components.ConfigProvider
 export declare const ConfirmBox: Components.ConfirmBox
+export declare const Content: Components.Content
 export declare const DatePicker: Components.DatePicker
 export declare const Dialog: Components.Dialog
 export declare const Drawer: Components.Drawer
 export declare const Dropdown: Components.Dropdown
+export declare const Empty: Components.Empty
 export declare const Field: Components.Field
 export declare const Fieldset: Components.Fieldset
 export declare const FilterPanel: Components.FilterPanel
+export declare const Footer: Components.Footer
 export declare const Form: Components.Form
 export declare const GridColumn: Components.GridColumn
 export declare const GridContainer: Components.GridContainer
 export declare const GridRow: Components.GridRow
+export declare const Header: Components.Header
 export declare const Icon: Components.Icon
 export declare const Input: Components.Input
 export declare const InputGroup: Components.InputGroup
 export declare const Label: Components.Label
+export declare const Layout: Components.Layout
 export declare const Lightbox: Components.Lightbox
 export declare const Link: Components.Link
 export declare const Loading: Components.Loading
-export declare const Menu: Components.Menu
 export declare const Message: Components.Message
 export declare const Nav: Components.Nav
 export declare const NumberInput: Components.NumberInput
@@ -54,6 +59,8 @@ export declare const RadioButtonGroup: Components.RadioButtonGroup
 export declare const RadioGroup: Components.RadioGroup
 export declare const SearchBox: Components.SearchBox
 export declare const Select: Components.Select
+export declare const Sidebar: Components.Sidebar
+export declare const Sidenav: Components.Sidenav
 export declare const Slider: Components.Slider
 export declare const Span: Components.Span
 export declare const Stack: Components.Stack
@@ -72,111 +79,6 @@ export declare const Tooltip: Components.Tooltip
 export declare const Transfer: Components.Transfer
 export declare const Tree: Components.Tree
 export declare const Uploader: Components.Uploader
-export declare const Content: Components.Content
-export declare const Footer: Components.Footer
-export declare const Header: Components.Header
-export declare const Layout: Components.Layout
-export declare const Sidebar: Components.Sidebar
-export declare const Sidenav: Components.Sidenav
-export declare const Empty: Components.Empty
 
-interface DialogManagerOptions {
-  overlayClass?: string | Array<string> | Record<string, boolean>
-  ok?(): Promise<void>
-}
-
-interface Statuses {
-  status?: 'success' | 'warning' | 'info' | 'error'
-  /** @deprecated */
-  type?: 'success' | 'warning' | 'info' | 'error'
-}
-
-declare class AlertDialogManager {
-  show(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions & Statuses
-  ): Promise<void>
-
-  success(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions
-  ): Promise<void>
-
-  info(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions
-  ): Promise<void>
-
-  error(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions
-  ): Promise<void>
-
-  warn(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions
-  ): Promise<void>
-}
-
-declare class ConfirmDialogManager<R> {
-  show(
-    content: string,
-    title: string,
-    options?: DialogManagerOptions & {
-      cancel(): Promise<void>
-    }
-  ): Promise<R>
-}
-
-interface ToastOptions {
-  duration?: number
-}
-
-declare class ToastDialogManager {
-  show(message: string, options?: ToastOptions & Statuses): void
-
-  success(message: string, options?: ToastOptions): void
-
-  info(message: string, options?: ToastOptions): void
-
-  error(message: string, options?: ToastOptions): void
-
-  warn(message: string, options?: ToastOptions): void
-}
-
-export declare const alert: AlertDialogManager
-export declare const confirm: ConfirmDialogManager<boolean>
-export declare const prompt: ConfirmDialogManager<string | null>
-export declare const toast: ToastDialogManager
-
-interface RuleOptions {
-  validate(value: unknown, ruleValue: unknown): boolean
-  message: string | ((value: unknown, ruleValue: unknown) => string)
-  priority: number
-}
-
-declare class RuleManager {
-  addRule(name: string, ruleOptions: RuleOptions): void
-}
-
-export declare const validation: RuleManager
-
-/* eslint-disable no-dupe-class-members */
-declare class ConfigManager {
-  defaults(key: string, value: unknown, ns?: string): void
-  defaults(values: Record<string, unknown>, ns?: string): void
-
-  set(key: string, val: unknown, ns?: string): void
-  set(values: Record<string, unknown>, ns?: string): void
-
-  get(path: string): unknown
-}
-/* eslint-enable no-dupe-class-members */
-
-export declare const config: ConfigManager
-export declare const ui: ConfigManager
+/** @deprecated */
+export declare const Menu: Components.Menu
---
+++ packages/veui/types/managers.d.ts
@@ -0,0 +1,171 @@
+interface DialogManagerOptions {
+  overlayClass?: string | Array<string> | Record<string, boolean>
+  ok?(): Promise<void>
+}
+
+interface Statuses {
+  status?: 'success' | 'warning' | 'info' | 'error'
+  /** @deprecated */
+  type?: 'success' | 'warning' | 'info' | 'error'
+}
+
+interface AlertDialogManager {
+  show(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions & Statuses
+  ): Promise<void>
+
+  success(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions
+  ): Promise<void>
+
+  info(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions
+  ): Promise<void>
+
+  error(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions
+  ): Promise<void>
+
+  warn(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions
+  ): Promise<void>
+}
+
+interface ConfirmDialogManager<R> {
+  show(
+    content: string,
+    title: string,
+    options?: DialogManagerOptions & {
+      cancel(): Promise<void>
+    }
+  ): Promise<R>
+}
+
+interface ToastOptions {
+  duration?: number
+}
+
+interface ToastDialogManager {
+  show(message: string, options?: ToastOptions & Statuses): void
+
+  success(message: string, options?: ToastOptions): void
+
+  info(message: string, options?: ToastOptions): void
+
+  error(message: string, options?: ToastOptions): void
+
+  warn(message: string, options?: ToastOptions): void
+}
+
+export declare const alert: AlertDialogManager
+export declare const confirm: ConfirmDialogManager<boolean>
+export declare const prompt: ConfirmDialogManager<string | null>
+export declare const toast: ToastDialogManager
+
+interface RuleOptions {
+  validate(value: unknown, ruleValue: unknown): boolean
+  message: string | ((value: unknown, ruleValue: unknown) => string)
+  priority: number
+}
+
+declare class RuleManager {
+  addRule(name: string, ruleOptions: RuleOptions): void
+}
+
+export declare const validation: RuleManager
+
+interface ConfigManager {
+  defaults(key: string, value: unknown, ns?: string): void
+  defaults(values: Record<string, unknown>, ns?: string): void
+
+  set(key: string, val: unknown, ns?: string): void
+  set(values: Record<string, unknown>, ns?: string): void
+
+  get(path: string): unknown
+}
+
+interface EnumPropDef {
+  values: string[]
+  default?: string
+}
+
+interface BooleanPropDef {
+  boolean: true
+}
+
+type PropDef = { inherited?: boolean } & (EnumPropDef | BooleanPropDef)
+type IconDef = string | unknown
+type PartDef = string | ((props: Record<string, unknown>) => string)
+
+interface PropDefs {
+  [key: string]: PropDef
+}
+
+interface IconDefs {
+  [key: string]: IconDef
+}
+
+interface PartDefs {
+  [key: string]: PartDef
+}
+
+type UIConfigKey = 'ui' | 'icons' | 'parts'
+
+type UIConfigMap<K extends UIConfigKey> = K extends 'ui'
+  ? PropDefs
+  : K extends 'icons'
+  ? IconDefs
+  : K extends 'parts'
+  ? PartDefs
+  : never
+
+type EnsureKey<T, U> = U extends string
+  ? T extends UIConfigKey
+    ? T
+    : never
+  : `${string}.${UIConfigKey}`
+
+type GetConfigKey<T> = T extends `${string}.${infer K}` ? K : never
+
+type UISetter = (<T, U>(
+  key: EnsureKey<T, U>,
+  value: T extends UIConfigKey
+    ? UIConfigMap<T>
+    : T extends `${string}.${infer K extends UIConfigKey}`
+    ? UIConfigMap<K>
+    : never,
+  ns?: (U & string) | undefined
+) => void) &
+  (<U>(
+    values: U extends string
+      ? {
+          [K in UIConfigKey]?: UIConfigMap<K>
+        }
+      : {
+          [K in `${string}.${UIConfigKey}`]?: UIConfigMap<GetConfigKey<K>>
+        },
+    ns?: (U & string) | undefined
+  ) => void)
+
+interface UIManager extends ConfigManager {
+  defaults: UISetter
+  set: UISetter
+  addPropValue(component: string, prop: string, value: string): void
+  setPropDefault(component: string, prop: string, value: string): void
+  setProp(component: string, prop: string, propDef: PropDef): void
+  setIcon(component: string, icon: string, iconDef: IconDef): void
+  setPart(component: string, part: string, partDef: PartDef): void
+}
+
+export declare const config: ConfigManager
+export declare const ui: UIManager
