1c9d7db236a831b8f8356cefbf09133403faee89
Justineo <justice360@gmail.com>
2023-09-11 18:34:44 +0800

    fix(types): add type for function columnFilter

--- packages/veui/types/components/table.d.ts
+++ packages/veui/types/components/table.d.ts
@@ -8,28 +8,28 @@ import { AllowedOrders } from './column'
 
 type Props<
   T extends Record<any, unknown>,
-  KeyField extends string | undefined = undefined
+  Field extends string | undefined = undefined
 > = (
   | {
       selectMode: 'single'
-      selected?: KeyField extends keyof T ? T[KeyField] : unknown
+      selected?: Field extends keyof T ? T[Field] : unknown
     }
   | {
       selectMode?: 'multiple'
-      selected?: KeyField extends keyof T ? Array<T[KeyField]> : Array<unknown>
+      selected?: Field extends keyof T ? Array<T[Field]> : Array<unknown>
     }
 ) & {
   data?: Array<T>
   scroll?: number | string | { x?: number | string; y?: number | string }
-  keyField?: KeyField
+  keyField?: Field
   selectable?: boolean
   expandable?: boolean
   bordered?: boolean
-  expanded?: KeyField extends keyof T ? Array<T[KeyField]> : Array<unknown>
+  expanded?: Field extends keyof T ? Array<T[Field]> : Array<unknown>
   order?: boolean | string
   orderBy?: string
   allowedOrders?: AllowedOrders
-  columnFilter?: Array<string>
+  columnFilter?: Array<Field> | ((field: Field) => boolean)
   loading?: boolean
 }
 
@@ -54,10 +54,10 @@ type Slots = {
 type Table = VeuiDefineComponent<{
   new <
     T extends Record<any, unknown> = {},
-    KeyField extends string | undefined = undefined
+    Field extends string | undefined = undefined
   >(
     ...args: any[]
-  ): VeuiDefineInstance<Props<T, KeyField>, Emits, Slots, Mixins>
+  ): VeuiDefineInstance<Props<T, Field>, Emits, Slots, Mixins>
 }>
 
 export default Table
